9dbec86298590bb0e22f232fc0e638b4700eac95
sxf <songfeixian@qq.com>
2017-06-22 16:33:07 +0800

    修改

--- fnsco-access-web/src/main/java/net/fnsco/controller/merchant/MerchantInfoController.java
+++ fnsco-access-web/src/main/java/net/fnsco/controller/merchant/MerchantInfoController.java
@@ -1,7 +1,5 @@
-/**
- * 
- */
-package net.fnsco.controller.merchant;
+package net.fnsco.controller.MerChant;
+
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
