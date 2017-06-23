0eef2a2c7dea0bcf446a974df3aa911f4408e789
jincheng.zhou <jincheng.zhou@zheft.cn>
2017-06-23 10:50:53 +0800

    用户注册

---
+++ fnsco-access-service/src/main/java/net/fnsco/service/modules/merchant/AppUserServiceImpl.java
@@ -0,0 +1,70 @@
+package net.fnsco.service.modules.merchant;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+import net.fnsco.api.merchant.AppUserService;
+import net.fnsco.core.base.BaseService;
+import net.fnsco.core.base.ResultDTO;
+import net.fnsco.service.dao.cluster.AppUserDao;
+import net.fnsco.service.domain.AppUser;
+
+@Service
+public class AppUserServiceImpl  extends BaseService implements AppUserService{
+	@Autowired
+	private AppUserDao MappUserDao;
+	//注册
+	@Override
+	public ResultDTO<AppUser> insertAppUser(AppUser appUser) {
+		ResultDTO<AppUser> result=new ResultDTO<AppUser>();
+		int num=MappUserDao.selectByPrimaryKey(appUser.getMobile());
+		if(num!=0){
+		      return result.setError("1","该用户已经注册");
+		}
+		if(MappUserDao.insertSelective(appUser)){
+			result.setData(appUser);
+			return result.setSuccess("注册成功");
+		}
+		return result;
+	}
+
+	//修改密码
+	@Override
+	public ResultDTO<AppUser> modify(HttpServletResponse res,HttpServletRequest req){
+		//旧密码
+		String oldPassword=req.getParameter("oldPassword");
+		//新密码
+		String password=req.getParameter("password");
+		String mobile=req.getParameter("mobile");
+		ResultDTO<AppUser> result=new ResultDTO<AppUser>();
+		AppUser mAppUser=new AppUser();
+		//根据手机号查询用户是否存在获取原密码
+		mAppUser=MappUserDao.selectByMobile(mobile);
+		//查到的密码和原密码做比较
+		if(!oldPassword.equals(mAppUser.getPassword())){
+			return result.setError("原密码输入错误,请重新输入");
+		}
+		mAppUser.setPassword(password);
+		mAppUser.setMobile(mobile);
+		if(MappUserDao.updateByPrimaryKeySelective(mAppUser)){
+			result.setSuccess("修改密码成功");
+		}
+		return result;
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
--- fnsco-access-web/src/main/java/net/fnsco/controller/AppUserController.java
+++ fnsco-access-web/src/main/java/net/fnsco/controller/AppUserController.java
@@ -1,31 +1,69 @@
 package net.fnsco.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+import net.fnsco.api.merchant.AppUserService;
+import net.fnsco.core.base.BaseController;
 import net.fnsco.core.base.ResultDTO;
 import net.fnsco.service.domain.AppUser;
-import net.fnsco.service.service.AppUserService;
 
 @RestController
-public class AppUserController {
+@RequestMapping("/app/user")
+public class AppUserController extends BaseController{
 	@Autowired
-	private AppUserService appUserService;
-	@RequestMapping(value = "/register", produces = "text/html;charset=UTF-8")
+	private AppUserService mAppUserService;
+	@RequestMapping(value = "/register")
+	@ApiOperation(value = "用户注册")
 	@ResponseBody
-	public ResultDTO<Object> register(HttpServletRequest req){
-		 //创建实体对象
+	public ResultDTO register(){
+		 String mobile=request.getParameter("mobile");
+		 String password=request.getParameter("password");
 		 AppUser appUser=new AppUser();
-		 String phonenum=req.getParameter("phonenum");
-	     String password=req.getParameter("password");
-	     appUser.setPhonenum(phonenum);
-	     appUser.setPassword(password);
-	     //调用service注册方法
-	     ResultDTO<Object> result =appUserService.insertAppUser(appUser);
-	     return result;
+		 appUser.setMobile(mobile);
+		 appUser.setPassword(password);
+		 //验证码验证
+		 
+		 //注册
+		 ResultDTO<AppUser> result=mAppUserService.insertAppUser(appUser);
+		 return result;
+	}
+	
+	//修改密码     旧密码和新密码
+	@RequestMapping(value = "/modify")
+	@ResponseBody
+	public ResultDTO<AppUser> modify(){
+		//创建实体对象
+		ResultDTO<AppUser> result = new ResultDTO<>();
+		//如果密码为空
+		String oldPassword=request.getParameter("oldPassword");
+		if(StringUtils.isEmpty(oldPassword)){
+			 return result.setError("密码不能为空");
+		 }
+		result=mAppUserService.modify(res, req);
+		return result;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
