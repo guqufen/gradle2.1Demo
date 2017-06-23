41a9f7e270153a3ea63f1efaa238e95f2e177eed
sxf <songfeixian@qq.com>
2017-06-23 14:55:05 +0800

    获取商户代码编码

--- fnsco-access-service/src/main/java/net/fnsco/service/modules/merchant/MerchantServiceImpl.java
+++ fnsco-access-service/src/main/java/net/fnsco/service/modules/merchant/MerchantServiceImpl.java
@@ -3,13 +3,20 @@
  */
 package net.fnsco.service.modules.merchant;
 
+import java.util.Date;
+import java.util.List;
+
+import org.assertj.core.internal.cglib.core.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import net.fnsco.api.merchant.MerchantService;
-import net.fnsco.core.base.ResultDTO;
-import net.fnsco.service.dao.master.MerchantFileDao;
-import net.fnsco.service.domain.MerchantFile;
+import net.fnsco.core.utils.DateUtils;
+import net.fnsco.service.dao.master.AliasDAO;
+import net.fnsco.service.dao.master.MerchantChannelDao;
+import net.fnsco.service.domain.Alias;
+import net.fnsco.service.domain.MerchantChannel;
+import net.fnsco.service.modules.helper.MerchantHelper;
 
 /**@desc 商户相关操作
  * @author sxfei
@@ -19,25 +26,47 @@ import net.fnsco.service.domain.MerchantFile;
 public class MerchantServiceImpl implements MerchantService {
 
     @Autowired
-    private MerchantFileDao merchantFileInfoDao;
+    private AliasDAO           aliasDAO;
+    @Autowired
+    private MerchantChannelDao merchantChannelDao;
 
     /**
-     * @todo 录入文件信息入库
-     * @author tangliang
-     * @date 2017年6月21日 上午10:17:44
-     * @see net.fnsco.api.merchant.MerchantInfoService#doAddToDB(net.fnsco.service.domain.MerchantFile, int)
-     */
-    public ResultDTO<Integer> doAddToDB(MerchantFile fileInfo, int loginUserId) {
-
-        ResultDTO<Integer> result = new ResultDTO<>();
-
-        int res_db = merchantFileInfoDao.insertSelective(fileInfo);
-
-        if (res_db != 1) {
-            result.setError();
-            return result.setError("添加失败");
+      * 
+      * @param merNum 商户号
+      * @param channelType
+      * @return
+      */
+    public String getMerCode(String merCode, String channelType) {
+        //商户代号
+        String randomCode = "";
+        MerchantChannel merchantChannel = merchantChannelDao.selectByMerCode(merCode, channelType);
+        String innerCode = merchantChannel.getInnerCode();
+        //查询所有的
+        List<Alias> aliasList = aliasDAO.selectByInnerCode(innerCode);
+        for (Alias alias : aliasList) {
+            randomCode = alias.getRandomCode();
+            Date validate = alias.getValidateTime();
+            if (validate.before(new Date())) {
+                return randomCode;
+            }
         }
-        return result.setSuccess("添加成功");
+        randomCode = MerchantHelper.getMerCode();
+        while (true) {
+            List<Alias> list = aliasDAO.selectByRandomCode(randomCode);
+            if (org.springframework.util.CollectionUtils.isEmpty(list)) {
+                break;
+            }
+            randomCode = MerchantHelper.getMerCode();
+        }
+        Alias alias = new Alias();
+        alias.setInnerCode(innerCode);
+        alias.setRandomCode(randomCode);
+        alias.setCreateTime(new Date());
+        alias.setDeadline(DateUtils.getDayEndTime(7));//+7天
+        alias.setValidateTime(DateUtils.getDayEndTime(6));//+6天
+        aliasDAO.insert(alias);
+
+        return randomCode;
     }
 
 }
--- fnsco-access-web/src/main/java/net/fnsco/controller/app/merchat/MerchantController.java
+++ fnsco-access-web/src/main/java/net/fnsco/controller/app/merchat/MerchantController.java
@@ -1,20 +1,21 @@
 package net.fnsco.controller.app.merchat;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
-import net.fnsco.api.trade.TradeDataService;
+import net.fnsco.api.merchant.MerchantService;
 import net.fnsco.core.base.BaseController;
 import net.fnsco.core.base.ResultDTO;
 
 @RestController
 @RequestMapping(value = "/app/merchant")
 public class MerchantController extends BaseController {
+    @Autowired
+    private MerchantService merchantService;
 
     /**
      * 保存拉卡拉交易数据到库
@@ -25,7 +26,7 @@ public class MerchantController extends BaseController {
     @RequestMapping(value = "/getMerCode", method = RequestMethod.GET)
     @ApiOperation(value = "获取商户编号")
     public ResultDTO getMerCode(@RequestParam String merCode, @RequestParam String channelType) {
-
-        return success();
+        String randomCode = merchantService.getMerCode(merCode, channelType);
+        return success(randomCode);
     }
 }
