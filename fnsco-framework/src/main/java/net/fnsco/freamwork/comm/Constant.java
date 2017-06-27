e616b4af5bad7e13d609cf6de1aa7e4eb10416b2
sxf <songfeixian@qq.com>
2017-06-27 14:58:02 +0800

    权限控制

--- fnsco-framework/src/main/java/net/fnsco/freamwork/comm/Constant.java
+++ fnsco-framework/src/main/java/net/fnsco/freamwork/comm/Constant.java
@@ -1,5 +1,16 @@
 package net.fnsco.freamwork.comm;
 
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+
 public class Constant {
-    public static String TOKEN_ID = "";
+    public static String              TOKEN_ID         = "b9eccb7036f9719059b0f61076991b75";
+    public static String              E_TOKEN_EMPTY    = "4000";
+    public static String              E_TOKEN_ERROR    = "4001";
+    public static Map<String, String> ERROR_MESSGE_MAP = Maps.newHashMap();
+    static {
+        ERROR_MESSGE_MAP.put("4000", "非法请求,没有token");
+        ERROR_MESSGE_MAP.put("4001", "非法请求,token不正确");
+    }
 }
