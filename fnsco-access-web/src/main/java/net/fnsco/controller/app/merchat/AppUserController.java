a44a095c6e1c72e1bf07fd01a10a960b825b59e2
jincheng.zhou <jincheng.zhou@zheft.cn>
2017-06-28 14:09:42 +0800

    注册和登录

--- fnsco-access-web/src/main/java/net/fnsco/controller/app/merchat/AppUserController.java
+++ fnsco-access-web/src/main/java/net/fnsco/controller/app/merchat/AppUserController.java
@@ -1,97 +1,68 @@
 package net.fnsco.controller.app.merchat;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
+import net.fnsco.api.dto.AppUserDTO;
 import net.fnsco.api.merchant.AppUserService;
 import net.fnsco.core.base.BaseController;
 import net.fnsco.core.base.ResultDTO;
 import net.fnsco.service.domain.AppUser;
-
+/**
+ *  by zhoujincheng
+ */
 @RestController
-@RequestMapping("/app/user")
+@RequestMapping(value="/app/user",method = RequestMethod.POST)
 public class AppUserController extends BaseController{
 	@Autowired
-	private AppUserService mAppUserService;
+	private AppUserService appUserService;
 	
 	@RequestMapping(value = "/register")
 	@ApiOperation(value = "用户注册")
 	@ResponseBody
-	public ResultDTO register(){
-		 String mobile=request.getParameter("mobile");
-		 String password=request.getParameter("password");
-		 String deviceId=request.getParameter("deviceId");
-		 String deviceToken=request.getParameter("deviceToken");
-		 int deviceType=Integer.parseInt(request.getParameter("deviceType"));
-		 String code=request.getParameter("code");
-		//创建实体对象
-		 AppUser appUser=new AppUser();
-		 appUser.setMobile(mobile);
-		 appUser.setPassword(password);
-		 appUser.setDeviceId(deviceId);
-		 appUser.setDeviceType(deviceType);
-		 appUser.setDeviceToken(deviceToken);
-		 //对比验证码
-		 ResultDTO<String> regResult=mAppUserService.validateCode(mobile, code);
-		 //如果错误返回错误信息   如果正确不用返回信息
-		 if(!regResult.isSuccess()){
-			 return regResult;
-		 }
-		 //验证码对比正确执行注册逻辑
-		 ResultDTO<AppUser> result=mAppUserService.insertAppUser(appUser);
+	public ResultDTO<AppUser> register(@RequestBody AppUserDTO appUserDTO){
+		 ResultDTO<AppUser> result=appUserService.insertAppUser(appUserDTO);
 		 return result;
 	}
 	
 	//获取验证码
 	@ResponseBody
-	@RequestMapping(value = "/getValidateCode", method = RequestMethod.GET)
+	@RequestMapping(value = "/getValidateCode")
 	@ApiOperation(value = "获取验证码")
-	public ResultDTO<String> getValidateCode(@RequestParam(value = "mobile", required = true) String mobile, @RequestParam(value = "deviceId", required = true) String deviceId, @RequestParam(value = "deviceType", required = true) int deviceType){
-		 ResultDTO<String> result=mAppUserService.getValidateCode(deviceId, deviceType, mobile);
-		 return result;
+	public void getValidateCode(@RequestBody AppUserDTO appUserDTO){
+		 appUserService.getValidateCode(appUserDTO);
 	}
 				
 	//修改密码     旧密码和新密码
-	@RequestMapping(value = "/modifyPassword", method = RequestMethod.GET)
+	@RequestMapping(value = "/modifyPassword")
 	@ResponseBody
 	@ApiOperation(value = "修改密码")
-	public ResultDTO<String> modifyPassword(@RequestParam(value = "id", required = true) int id,@RequestParam(value = "oldPassword", required = true) String oldPassword,@RequestParam(value = "password", required = true) String password){
+	public ResultDTO<String> modifyPassword(@RequestBody AppUserDTO appUserDTO){
 		ResultDTO<String> result = new ResultDTO<>();
-		//如果密码为空
-		if(StringUtils.isEmpty(oldPassword)){
-			 return result.setError("密码不能为空");
-		 }
-		result=mAppUserService.modifyPassword(id,password,oldPassword);
+		result=appUserService.modifyPassword(appUserDTO);
 		return result;
 	}
 	
 	//根据手机号码找回密码
 		@ResponseBody
-		@RequestMapping(value = "/findPasswordByPhone",method = RequestMethod.GET)
+		@RequestMapping(value = "/findPasswordByPhone")
 		@ApiOperation(value = "找回密码")
-		public ResultDTO<String> findPasswordByPhone(@RequestParam(value = "mobile", required = true) String mobile, @RequestParam(value = "password", required = true) String password, @RequestParam(value = "code", required = true) String code){
-			//对比验证码
-			ResultDTO<String> regResult=mAppUserService.validateCode(mobile, code);
-			//如果错误返回错误信息
-			 if(!regResult.isSuccess()){
-				 return regResult;
-			 }
-			ResultDTO<String> result=mAppUserService.findPassword(mobile,password);
+		public ResultDTO<String> findPasswordByPhone(@RequestBody AppUserDTO appUserDTO){
+			ResultDTO<String> result=appUserService.findPassword(appUserDTO);
 			return result;
 		}
 		
 		//登录
 		@ResponseBody
-		@RequestMapping(value = "/login",method = RequestMethod.GET)
+		@RequestMapping(value = "/login")
 		@ApiOperation(value = "用户登录")
-		public ResultDTO<String> login(@RequestParam(value = "mobile", required = true) String mobile,@RequestParam(value = "password", required = true) String password){
-			ResultDTO<String> result=mAppUserService.loginByMoblie(mobile, password);
+		public ResultDTO<String> login(@RequestBody AppUserDTO appUserDTO){
+			ResultDTO<String> result=appUserService.loginByMoblie(appUserDTO);
 			return result;
 		}
 		
--- fnsco-access-web/src/main/java/net/fnsco/controller/app/merchat/test.java
+++ fnsco-access-web/src/main/java/net/fnsco/controller/app/merchat/test.java
@@ -1,17 +1,59 @@
 package net.fnsco.controller.app.merchat;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 String sendCode="111111_22222";
-		 String missM = sendCode.substring(sendCode.lastIndexOf("_")+1, sendCode.length());
-			
-		long oldTime = Long.valueOf(missM);
-		System.out.println(oldTime);
-			
+			Map<String,Emp> map=new HashMap();
+			String id="id";
+	        Emp emp=new Emp();
+	        emp.setE_id("id");
+	        emp.setE_name("name");
+	        map.put(id, emp);
+	        map.get(emp.getE_id());
+	        System.out.println(map.get(id).getE_id());
+	        System.out.println(map.get(id).getE_name());
+//	        Set<String> keys=map.keySet();
+//	        System.out.println("遍历集合map：");
+//	        System.out.println(map.get("005"));
+//	        Iterator<String> it=keys.iterator();
+//	        while(it.hasNext())
+//	        {
+//	            String key=it.next();
+//	            System.out.println(key+" "+map.get(key));
+//	        }
+	      //  System.out.println("移除的是"+map.remove("005"));
+		
 	}
 
 }
+
+
+
+ class Emp {
+    private String e_id;
+    private String e_name;
+//    public Emp(String e_id, String e_name) {
+//        super();
+//        this.e_id = e_id;
+//        this.e_name = e_name;
+//    }
+    public String getE_id() {
+        return e_id;
+    }
+    public void setE_id(String e_id) {
+        this.e_id = e_id;
+    }
+    public String getE_name() {
+        return e_name;
+    }
+    public void setE_name(String e_name) {
+        this.e_name = e_name;
+    }
+ }
+    
