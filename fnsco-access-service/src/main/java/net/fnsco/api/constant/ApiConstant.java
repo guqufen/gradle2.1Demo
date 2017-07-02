d1affb023f2818ffe438eb218f58c74a81b0f53e
jincheng.zhou <jincheng.zhou@zheft.cn>
2017-07-02 17:08:50 +0800

    注册

--- fnsco-access-service/src/main/java/net/fnsco/api/constant/ApiConstant.java
+++ fnsco-access-service/src/main/java/net/fnsco/api/constant/ApiConstant.java
@@ -1,8 +1,5 @@
 package net.fnsco.api.constant;
 
-import java.util.Map;
-
-import com.google.common.collect.Maps;
 
 import net.fnsco.core.constants.CoreConstants;
 
@@ -50,36 +47,36 @@ public class ApiConstant extends CoreConstants {
     public static String E_UPDATEPASSWORD_ERROR    = "5107";                            //更新密码失败
     public static String E_OLDPASSWORD_ERROR       = "5105";                            //  原密码错误
     public static String E_NOREGISTER_LOGIN        = "5106";                            //用户未注册
-    //未注册   5106
+    
     
     static {
         //APP
         ERROR_MESSGE_MAP.put(E_USERID_NULL, "入参ID为null");
         ERROR_MESSGE_MAP.put(E_UPDATE_FAIL, "更新失败");
-        ERROR_MESSGE_MAP.put("5011", "非法请求,没有手机号");
-        ERROR_MESSGE_MAP.put("5012", "非法请求,手机号不正确");
-        ERROR_MESSGE_MAP.put("5013", "非法请求,没有密码");
-        ERROR_MESSGE_MAP.put("5014", "非法请求,密码不正确");
-        ERROR_MESSGE_MAP.put("5015", "非法请求,没有id");
-        ERROR_MESSGE_MAP.put("5016", "非法请求,id不正确");
-        ERROR_MESSGE_MAP.put("5017", "非法请求,没有deviceType");
-        ERROR_MESSGE_MAP.put("5018", "非法请求,deviceType不正确");
-        ERROR_MESSGE_MAP.put("5019", "非法请求,没有验证码");
-        ERROR_MESSGE_MAP.put("5020", "非法请求,验证码不正确");
-        ERROR_MESSGE_MAP.put("5021", "非法请求,设备号为空");
-        ERROR_MESSGE_MAP.put("5022", "非法请求,设备号不正确");
+        ERROR_MESSGE_MAP.put(E_APP_PHONE_EMPTY, "非法请求,没有手机号");
+        ERROR_MESSGE_MAP.put(E_APP_PHONE_ERROR, "非法请求,手机号不正确");
+        ERROR_MESSGE_MAP.put(E_APP_PASSWORD_EMPTY, "非法请求,没有密码");
+        ERROR_MESSGE_MAP.put(E_APP_PASSWORD_ERROR, "非法请求,密码不正确");
+        ERROR_MESSGE_MAP.put(E_APP_ID_EMPTY, "非法请求,没有id");
+        ERROR_MESSGE_MAP.put(E_APP_ID_ERROR, "非法请求,id不正确");
+        ERROR_MESSGE_MAP.put(E_APP_DEVICETYPE_EMPTY, "非法请求,没有deviceType");
+        ERROR_MESSGE_MAP.put(E_APP_DEVICETYPE_ERROR, "非法请求,deviceType不正确");
+        ERROR_MESSGE_MAP.put(E_APP_CODE_EMPTY, "非法请求,没有验证码");
+        ERROR_MESSGE_MAP.put(E_APP_CODE_ERROR, "非法请求,验证码不正确");
+        ERROR_MESSGE_MAP.put(E_APP_DEVICEIDNULL, "非法请求,设备号为空");
+        ERROR_MESSGE_MAP.put(E_APP_DEVICETYPENULL, "非法请求,设备号不正确");
 
-        ERROR_MESSGE_MAP.put("5101", "用户已注册");
-        ERROR_MESSGE_MAP.put("5102", "注册失败");
-        ERROR_MESSGE_MAP.put("5103", "验证码超时");
-        ERROR_MESSGE_MAP.put("5107", "更新密码失败");
-        ERROR_MESSGE_MAP.put("5105", "原密码错误");
-        ERROR_MESSGE_MAP.put("5106", "用户未注册");
+        ERROR_MESSGE_MAP.put(E_ALREADY_LOGIN, "用户已注册");
+        ERROR_MESSGE_MAP.put(E_REGISTER_ERROR, "注册失败");
+        ERROR_MESSGE_MAP.put(E_CODEOVERTIME_ERROR, "验证码超时");
+        ERROR_MESSGE_MAP.put(E_UPDATEPASSWORD_ERROR, "更新密码失败");
+        ERROR_MESSGE_MAP.put(E_OLDPASSWORD_ERROR, "原密码错误");
+        ERROR_MESSGE_MAP.put(E_NOREGISTER_LOGIN, "用户未注册");
 
-        ERROR_MESSGE_MAP.put("5023", "商铺码不能为空");
-        ERROR_MESSGE_MAP.put("5024", "用户ID不能为空");
-        ERROR_MESSGE_MAP.put("5025", "此商铺码不存在，请重新输入");
-        ERROR_MESSGE_MAP.put("5026", "此商铺码已过期，请到pos机查询最新的商铺码");
+        ERROR_MESSGE_MAP.put(E_MERCHANT_CODE_NULL, "商铺码不能为空");
+        ERROR_MESSGE_MAP.put(E_USER_ID_NULL, "用户ID不能为空");
+        ERROR_MESSGE_MAP.put(E_MERCHANT_CODE_NOT_EXIST, "此商铺码不存在，请重新输入");
+        ERROR_MESSGE_MAP.put(E_MERCHANT_CODE_OVERDUE, "此商铺码已过期，请到pos机查询最新的商铺码");
     }
 
 }
