e98bc239b7073dcdab278c8a96725011021f88cf
sxf <songfeixian@qq.com>
2017-07-02 16:25:46 +0800

    交易流水查询

--- fnsco-access-service/src/main/java/net/fnsco/api/dto/TradeDataQueryDTO.java
+++ fnsco-access-service/src/main/java/net/fnsco/api/dto/TradeDataQueryDTO.java
@@ -3,20 +3,20 @@ package net.fnsco.api.dto;
 import net.fnsco.core.base.PageDTO;
 
 public class TradeDataQueryDTO extends PageDTO {
-    private String userId;    //登录用户Id
-    private String innerCode; // 内部商务号
-    private String startDate; // 开始日期 2016-5-10
-    private String endDate;   //结束日期 2016-5-10
-    private String terminals; //终端号数组，所有选择的终端
-    private String tradeId;   //交易流水ID
-    
+    private Integer userId;    //登录用户Id
+    private String  innerCode; // 内部商务号
+    private String  startDate; // 开始日期 2016-05-10
+    private String  endDate;   //结束日期 2016-05-10
+    private String  terminals; //终端号数组，所有选择的终端
+    private String  tradeId;   //交易流水ID
+
     /**
      * tradeId
      *
      * @return  the tradeId
      * @since   CodingExample Ver 1.0
     */
-    
+
     public String getTradeId() {
         return tradeId;
     }
@@ -27,7 +27,7 @@ public class TradeDataQueryDTO extends PageDTO {
      * @param   tradeId    the tradeId to set
      * @since   CodingExample Ver 1.0
      */
-    
+
     public void setTradeId(String tradeId) {
         this.tradeId = tradeId;
     }
@@ -39,7 +39,7 @@ public class TradeDataQueryDTO extends PageDTO {
      * @since   CodingExample Ver 1.0
     */
 
-    public String getUserId() {
+    public Integer getUserId() {
         return userId;
     }
 
@@ -50,7 +50,7 @@ public class TradeDataQueryDTO extends PageDTO {
      * @since   CodingExample Ver 1.0
      */
 
-    public void setUserId(String userId) {
+    public void setUserId(Integer userId) {
         this.userId = userId;
     }
 
