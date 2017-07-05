26e56d03eed6b1fea5b66c09eadac7c9665067d1
TangLiang <liang.tang@zheft.cn>
2017-07-05 11:45:05 +0800

    提交BUG

--- fnsco-access-web/src/main/java/net/fnsco/controller/web/sysuser/SysUserLoginAction.java
+++ fnsco-access-web/src/main/java/net/fnsco/controller/web/sysuser/SysUserLoginAction.java
@@ -44,7 +44,7 @@ public class SysUserLoginAction extends BaseController{
 		String username = req.getParameter("account");
 		String password = req.getParameter("password");
 		if (StringUtils.isEmpty(password)) {
-            return "forward:/";
+            return "redirect:/login.html";
         }
 		
 		result = adminUserService.doLogin(res, username, password);
@@ -52,7 +52,7 @@ public class SysUserLoginAction extends BaseController{
 			setSessionUser(result.getData());
 			CookieUtils.addCookie(res, CoreConstants.COOKIE_USER_KEY, ((SysUser)result.getData()).getName());
 		}
-		return "forward:/";
+		return "redirect:/index.html";
 	}
 	
 	/**
