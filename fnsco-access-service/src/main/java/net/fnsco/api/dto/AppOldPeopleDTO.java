3c21a9e16e5b4b6046480d86a9e0872f27cc16c8
jincheng.zhou <jincheng.zhou@zheft.cn>
2017-07-21 10:24:14 +0800

    角色

--- fnsco-access-service/src/main/java/net/fnsco/api/dto/AppOldPeopleDTO.java
+++ fnsco-access-service/src/main/java/net/fnsco/api/dto/AppOldPeopleDTO.java
@@ -6,6 +6,13 @@ import net.fnsco.core.base.DTO;
 
 public class AppOldPeopleDTO extends DTO{
     private List<AppOldListDTO> list;
+    private List<AppOldListDTO> clerkList;
+    public List<AppOldListDTO> getClerkList() {
+        return clerkList;
+    }
+    public void setClerkList(List<AppOldListDTO> clerkList) {
+        this.clerkList = clerkList;
+    }
     public List<AppOldListDTO> getList() {
         return list;
     }
