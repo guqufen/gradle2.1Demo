2a4685bfdf361c9437c80baf03c7e97d02353469
sxf <songfeixian@qq.com>
2017-07-31 09:22:58 +0800

    回调修改

--- fnsco-withhold-service/src/main/java/net/fnsco/withhold/web/open/PayCallback.java
+++ fnsco-withhold-service/src/main/java/net/fnsco/withhold/web/open/PayCallback.java
@@ -1,5 +1,8 @@
 package net.fnsco.withhold.web.open;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,18 +10,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import net.fnsco.core.base.BaseController;
 import net.fnsco.core.base.ResultDTO;
 
 @Controller
 @RequestMapping(value = "/open", method = RequestMethod.POST)
 @Api(value = "/open", tags = { "支付回调接口" })
-public class PayCallback {
+public class PayCallback extends BaseController {
     // 分页
     @ApiOperation(value = "浦发代收回调接口", notes = "浦发代收回调接口")
     @ResponseBody
     @RequestMapping(value = "/pfWithholdCallback", method = RequestMethod.GET)
     public ResultDTO pfWithholdCallback() {
-
+        Map<String, String> parameterMap = getParameterMap();
+         
         return ResultDTO.success();
     }
 }
