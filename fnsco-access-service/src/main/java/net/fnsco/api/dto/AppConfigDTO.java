e7d953a27fa29128fae4255bdcd51f1af3ad61c9
TangLiang <liang.tang@zheft.cn>
2017-08-02 16:43:10 +0800

    增加获取地址的代码

---
+++ fnsco-access-service/src/main/java/net/fnsco/api/dto/AppConfigDTO.java
@@ -0,0 +1,71 @@
+package net.fnsco.api.dto;
+
+import net.fnsco.core.base.DTO;
+
+/**
+ * @desc
+ * @author   tangliang
+ * @version  0.0.1-SNAPSHOT
+ * @since    Ver 1.1
+ * @Date	 2017年8月2日 下午3:12:21
+ */
+
+public class AppConfigDTO extends DTO {
+
+    /**
+     * serialVersionUID:TODO（用一句话描述这个变量表示什么）
+     *
+     * @since Ver 1.1
+     */
+    
+    private static final long serialVersionUID = 6075033483631903359L;
+    /**
+     * 用户ID
+     */
+    private Integer userId;
+    /**
+     * 类型
+     */
+    private String type;
+    /**
+     * userId
+     *
+     * @return  the userId
+     * @since   CodingExample Ver 1.0
+    */
+    
+    public Integer getUserId() {
+        return userId;
+    }
+    /**
+     * userId
+     *
+     * @param   userId    the userId to set
+     * @since   CodingExample Ver 1.0
+     */
+    
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+    /**
+     * type
+     *
+     * @return  the type
+     * @since   CodingExample Ver 1.0
+    */
+    
+    public String getType() {
+        return type;
+    }
+    /**
+     * type
+     *
+     * @param   type    the type to set
+     * @since   CodingExample Ver 1.0
+     */
+    
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+}
--- fnsco-access-web/src/main/java/net/fnsco/controller/app/trade/TradeReportController.java
+++ fnsco-access-web/src/main/java/net/fnsco/controller/app/trade/TradeReportController.java
@@ -1,15 +1,11 @@
 package net.fnsco.controller.app.trade;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.google.common.base.Strings;
-
 import io.swagger.annotations.ApiOperation;
 import net.fnsco.api.constant.ApiConstant;
 import net.fnsco.api.dto.TradeReportDTO;
@@ -91,4 +87,6 @@ public class TradeReportController extends BaseController{
         WeeklyHisDateDTO datas = tradeReportService.queryWeeklyHisDate(tradeReportDTO);
         return ResultDTO.success(datas);
     }
+    
+    
 }
