fc958d8e20e826e331af89665cae5b18d0bfe985
sxf <songfeixian@qq.com>
2017-08-22 11:19:50 +0800

    权限限制

--- fnsco-service-auth/src/main/java/net/fnsco/auth/config/ShiroConfig.java
+++ fnsco-service-auth/src/main/java/net/fnsco/auth/config/ShiroConfig.java
@@ -74,8 +74,8 @@ public class ShiroConfig {
         filterMap.put("/favicon.ico", "anon");
         filterMap.put("/captcha.jpg", "anon");
         filterMap.put("/", "anon");
-        filterMap.put("/**/*", "anon");
-        filterMap.put("/*", "anon");
+        //filterMap.put("/**/*", "anon");
+        //filterMap.put("/*", "anon");
         filterMap.put("/web/**", "oauth2");
         shiroFilter.setFilterChainDefinitionMap(filterMap);
 
