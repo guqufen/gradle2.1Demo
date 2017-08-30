5798b4b61c771e00ad34a37a3b3949c19e301e19
sxf <songfeixian@qq.com>
2017-08-30 16:50:53 +0800

    修改配置

---
+++ fnsco-service-bigdata/src/main/java/net/fnsco/bigdata/api/dto/TradeDataPageDTO.java
@@ -0,0 +1,68 @@
+package net.fnsco.bigdata.api.dto;
+
+import java.util.List;
+
+import net.fnsco.core.base.DTO;
+
+public class TradeDataPageDTO<T> extends DTO {
+    public TradeDataPageDTO() {
+
+    }
+
+    public TradeDataPageDTO(int total, List<T> list) {
+        this.list = list;
+        this.total = total;
+    }
+
+    private static final long serialVersionUID = -7387542509934814087L;
+    private List<T>           list;
+    private int               total;
+    private int               currentPage;
+    private int               merTotal;
+    
+    /**
+     * merTotal
+     *
+     * @return  the merTotal
+     * @since   CodingExample Ver 1.0
+    */
+    
+    public int getMerTotal() {
+        return merTotal;
+    }
+
+    /**
+     * merTotal
+     *
+     * @param   merTotal    the merTotal to set
+     * @since   CodingExample Ver 1.0
+     */
+    
+    public void setMerTotal(int merTotal) {
+        this.merTotal = merTotal;
+    }
+
+    public List<T> getList() {
+        return this.list;
+    }
+
+    public void setList(List<T> list) {
+        this.list = list;
+    }
+
+    public int getTotal() {
+        return this.total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getCurrentPage() {
+        return this.currentPage;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
+}
--- fnsco-war-timer/src/main/java/net/fnsco/TomcatApplication.java
+++ fnsco-war-timer/src/main/java/net/fnsco/TomcatApplication.java
@@ -18,7 +18,7 @@ import net.fnsco.freamwork.log.filter.WebAccessLogFilter;
 @SpringBootApplication
 @ComponentScan("net.fnsco")
 @EntityScan("net.fnsco.order.service.dao")
-@MapperScan("net.fnsco.auth.service.sys.dao")
+@MapperScan({"net.fnsco.auth.service","net.fnsco.order.service","net.fnsco.bigdata.service"})
 @Import({ TimerConfig.class, WebConfig.class })
 public class TomcatApplication extends SpringBootServletInitializer {
 	
