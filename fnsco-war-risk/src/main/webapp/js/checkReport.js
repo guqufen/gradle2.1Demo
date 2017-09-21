b779fc2a14303f3f16e35c5b96e03aaf138dabd0
jincheng.zhou <jincheng.zhou@zheft.cn>
2017-09-21 13:29:59 +0800

    风控报告

---
+++ fnsco-war-risk/src/main/webapp/js/checkReport.js
@@ -0,0 +1,66 @@
+//获取商户id
+function GetRequest() {
+	var url = location.search; //获取url中"?"符后的字串   
+	var theRequest = new Object();
+	if (url.indexOf("?") != -1) {
+		var str = url.substr(1);
+		strs = str.split("&");
+		for (var i = 0; i < strs.length; i++) {
+			theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
+		}
+	}
+	return theRequest;
+}
+var Request = new Object();
+Request = GetRequest();
+var merchantId=Request["merchantId"];
+//获取用户信息
+function load_val2() {
+	var result;
+	$.ajax({
+		dataType : 'json',
+		type : 'POST',
+		url : PROJECT_NAME + '/web/user/getCurrentUser',
+		async : false,// 这里选择异步为false，那么这个程序执行到这里的时候会暂停，等待
+		// 数据加载完成后才继续执行
+		success : function(data) {
+			console.log(data)
+			result = data.data;
+		}
+	});
+	return result;
+}
+var message = load_val2();
+console.log(message.id)
+var webUserOuterId = message.id;
+//获取风控报告明细
+$(function(){
+	$.ajax({
+	url : PROJECT_NAME + '/web/report/queryReportDetails',
+	type : 'POST',
+	dataType : "json",
+	data : {"merchantId":merchantId},
+	success : function(data){
+		var result=data.data;
+		$(".merName span").html(result.merName);
+		$(".businessLicenseNum span").html(result.businessLicenseNum);
+		$(".businessAddress span").html(result.businessAddress);
+		$(".businessDueTime span").html(result.businessDueTime);
+		$(".industry span").html(result.industry);
+		$(".size span").html(result.size);
+		$(".merName span").html(result.merName);
+		$(".riskWarning ").html(result.riskWarning);
+	}
+});
+})
+
+//查询全年风控曲线图
+$.ajax({
+	url : PROJECT_NAME + '/web/report/queryYearReport',
+	type : 'POST',
+	dataType : "json",
+	data : {"userId":webUserOuterId,"merchantId":merchantId},
+	success : function(data){
+		console.log(data)
+	}
+});
