4ba5062813cb2f486ef3fc60979f5c78dc771f74
jincheng.zhou <jincheng.zhou@zheft.cn>
2017-09-25 14:51:48 +0800

    邮箱

--- fnsco-service-risk/src/main/java/net/fnsco/risk/service/report/ReportService.java
+++ fnsco-service-risk/src/main/java/net/fnsco/risk/service/report/ReportService.java
@@ -221,7 +221,7 @@ public class ReportService extends BaseService {
             MimeMessageHelper helper = new MimeMessageHelper(message, true);
             System.out.println(ev.getProperty("username"));
             helper.setFrom("fanaisheng@zheft.cn");
-            helper.setTo("782430551@qq.com");
+            helper.setTo("1606646959@qq.com");
             helper.setSubject("风控报告");
             StringBuffer sb = new StringBuffer();
             sb.append("<div style='font-size:26px;margin-top:50px;'>"+dto.getName() + "申请生成关于" + reportInfoDO.getMerName() + "的风控报告,请尽快处理" + "<a href='http://www.w3school.com.cn'>W3School</a></div>");
