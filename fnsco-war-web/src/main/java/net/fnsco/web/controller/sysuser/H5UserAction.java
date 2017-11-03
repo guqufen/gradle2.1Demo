445659c85d388ca23b25b91ae7221837ed221a3e
liang.tang <liang.tang@zheft.cn>
2017-11-03 11:19:47 +0800

    提交代码

--- fnsco-war-web/src/main/java/net/fnsco/web/controller/sysuser/H5UserAction.java
+++ fnsco-war-web/src/main/java/net/fnsco/web/controller/sysuser/H5UserAction.java
@@ -10,11 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.common.base.Strings;
+
 import io.swagger.annotations.ApiOperation;
 import net.fnsco.core.base.BaseController;
 import net.fnsco.core.base.ResultDTO;
 import net.fnsco.order.api.appuser.AppUserService;
 import net.fnsco.order.api.dto.AppUserDTO;
+import net.fnsco.order.api.merchant.IntegralRuleLogService;
+import net.fnsco.order.service.domain.IntegralRuleLog;
 
 /**@desc 后台管理系统登录控制器
  * @author tangliang
@@ -26,12 +30,20 @@ public class H5UserAction extends BaseController {
 
     @Autowired
     private AppUserService appUserService;
+    
+    @Autowired
+    private IntegralRuleLogService integralRuleLogService;
 
     @RequestMapping(value = "/register")
     @ApiOperation(value = "用户注册")
     @ResponseBody
     public ResultDTO register(@RequestBody AppUserDTO appUserDTO) {
         ResultDTO result = appUserService.insertSelective(appUserDTO);
+        if(Strings.isNullOrEmpty(appUserDTO.getEntityInnerCode())) {
+        	logger.info(appUserDTO.getMobile()+"该手机号注册没有实体商户邀请!");
+        }else {
+        	integralRuleLogService.insert(appUserDTO.getEntityInnerCode(), IntegralRuleLog.IntegralTypeEnum.CODE_YQ02.getCode());
+        }
         return result;
     }
 
