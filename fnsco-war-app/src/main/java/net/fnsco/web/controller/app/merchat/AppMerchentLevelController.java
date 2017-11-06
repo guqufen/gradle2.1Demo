885eb0cfb1b0bd231e78197e5f3575bf9d8af872
icy-dust@qq.com <12345678>
2017-11-06 15:14:53 +0800

    修改

--- fnsco-war-app/src/main/java/net/fnsco/web/controller/app/merchat/AppMerchentLevelController.java
+++ fnsco-war-app/src/main/java/net/fnsco/web/controller/app/merchat/AppMerchentLevelController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.common.collect.Lists;
 
 import io.swagger.annotations.Api;
 import net.fnsco.bigdata.api.constant.BigdataConstant;
