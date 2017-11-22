2f9aac4bf858077c46f7b3e4bcb9e158d95949b2
lbh <binghui.li@zheft.cn>
2017-11-22 14:05:46 +0800

    修改显示通道页面信息

--- fnsco-service-bigdata/src/main/java/net/fnsco/bigdata/service/modules/merchant/MerchantCoreServiceImpl.java
+++ fnsco-service-bigdata/src/main/java/net/fnsco/bigdata/service/modules/merchant/MerchantCoreServiceImpl.java
@@ -241,13 +241,14 @@ public class MerchantCoreServiceImpl implements MerchantCoreService {
             return result.fail();
         }
         List<MerchantChannel> channelList = core.getChannel();
-//        if(CollectionUtils.isEmpty(channelList)){
-//        	channelList = Lists.newArrayList();
-//        	MerchantChannel channel = new MerchantChannel();
-//        	channel.setId(0);
-//        	channelList.add(channel);
-//        	core.setChannel(channelList);
-//        }
+        if(CollectionUtils.isEmpty(channelList)){
+        	channelList = Lists.newArrayList();
+        	MerchantChannel channel = new MerchantChannel();
+        	channel.setId(0);
+        	channel.setInnerCode(core.getInnerCode());
+        	channelList.add(channel);
+        	core.setChannel(channelList);
+        }
       //查询名称
         if(!Strings.isStringEmpty(core.getInnerCode())) {
         	MerchantEntity merEntity = merchantEntityDao.queryMerEntityByInnerCode(core.getInnerCode());
