dbc9c865ae59b6eee7c54623f20836f555b91c68
sxf <songfeixian@qq.com>
2017-11-23 16:52:31 +0800

    拉卡拉数据同步修改

--- fnsco-service-bigdata/src/main/java/net/fnsco/bigdata/service/dao/master/MerchantPosDao.java
+++ fnsco-service-bigdata/src/main/java/net/fnsco/bigdata/service/dao/master/MerchantPosDao.java
@@ -9,6 +9,7 @@ import net.fnsco.bigdata.api.dto.PosInfoDTO;
 import net.fnsco.bigdata.api.dto.PosInfosDTO;
 import net.fnsco.bigdata.api.dto.PosListDTO;
 import net.fnsco.bigdata.service.domain.MerchantPos;
+
 /**
  * @desc POS机信息DAO
  * @author   tangliang
@@ -19,6 +20,7 @@ import net.fnsco.bigdata.service.domain.MerchantPos;
 public interface MerchantPosDao {
 
     int deleteByPrimaryKey(Integer id);
+
     /**
      * deleteByChannelId:(这里用一句话描述这个方法的作用)根据channelId删除
      * @param channelId
@@ -27,8 +29,8 @@ public interface MerchantPosDao {
      * @date      2017年8月17日 下午1:41:59
      * @return int    DOM对象
      */
-    int deleteByChannelId(@Param("channelId")Integer channelId);
-    
+    int deleteByChannelId(@Param("channelId") Integer channelId);
+
     /**
      * deleteByMerCoreIds:( 根据core实体IDS删除关联数据)
      * @param ids
@@ -40,7 +42,7 @@ public interface MerchantPosDao {
     int deleteByMerCoreIds(Integer[] ids);
 
     int insert(MerchantPos record);
-    
+
     Integer insertPos(MerchantPos record);
 
     int insertSelective(MerchantPos record);
@@ -50,7 +52,7 @@ public interface MerchantPosDao {
     int updateByPrimaryKeySelective(MerchantPos record);
 
     int updateByPrimaryKey(MerchantPos record);
-    
+
     /**
      * selectAllByUserId:(这里用一句话描述这个方法的作用)根据userID查询列表
      * @param userId
@@ -59,7 +61,8 @@ public interface MerchantPosDao {
      * @date      2017年8月18日 下午4:58:22
      * @return List<PosInfoDTO>    DOM对象
      */
-    List<PosInfoDTO> selectAllByUserId(@Param("userId")Integer userId);
+    List<PosInfoDTO> selectAllByUserId(@Param("userId") Integer userId);
+
     /**
      * selectByChannelId:(这里用一句话描述这个方法的作用)根据channelId查询
      * @param channelId
@@ -68,8 +71,8 @@ public interface MerchantPosDao {
      * @date      2017年8月17日 下午2:44:43
      * @return List<MerchantPos>    DOM对象
      */
-    List<MerchantPos> selectByChannelId(@Param("channelId")Integer channelId);
-    
+    List<MerchantPos> selectByChannelId(@Param("channelId") Integer channelId);
+
     /**
      * selectPosNamesByInnerCode:(这里用一句话描述这个方法的作用)根据innercode查询出 POS姓名
      * @param innerCode
@@ -78,8 +81,8 @@ public interface MerchantPosDao {
      * @date      2017年8月16日 下午1:27:24
      * @return List<String>    DOM对象
      */
-    List<PosInfosDTO> selectPosNamesByInnerCode(@Param("innerCode")String innerCode);
-    
+    List<PosInfosDTO> selectPosNamesByInnerCode(@Param("innerCode") String innerCode);
+
     /**
      * selectAllPosInfo:(这里用一句话描述这个方法的作用) 根据用户ID查询所有POS机设备
      * @param userId
@@ -88,8 +91,8 @@ public interface MerchantPosDao {
      * @date      2017年8月16日 下午1:33:08
      * @return List<PosListDTO>    DOM对象
      */
-    List<PosListDTO> selectAllPosInfo(@Param("userId")Integer userId);
-    
+    List<PosListDTO> selectAllPosInfo(@Param("userId") Integer userId);
+
     /**
      * selectDetailById:(这里用一句话描述这个方法的作用)根据ID查询详情
      * @param id
@@ -99,6 +102,7 @@ public interface MerchantPosDao {
      * @return PosDetailDTO    DOM对象
      */
     PosDetailDTO selectDetailById(Integer id);
+
     /**
      * 
      * selectBySnCode:(根据snCode查询pos列表)
@@ -109,9 +113,9 @@ public interface MerchantPosDao {
      * @since  CodingExample　Ver 1.1
      */
     List<MerchantPos> selectBySnCode(@Param("snCode") String snCode);
-    
-    
+
     List<MerchantPos> selectByInnerCode(@Param("innerCode") String innerCode);
+
     /**
      * selectBySnCodeAndInnerCode:(通过sncode和innercode查询)
      * @return    设定文件
@@ -119,8 +123,8 @@ public interface MerchantPosDao {
      * @date      2017年9月22日 上午11:41:05
      * @return MerchantPos    DOM对象
      */
-    MerchantPos selectBySnCodeAndInnerCode(@Param("snCode")String snCode,@Param("innerCode")String innerCode,@Param("channelId")Integer channelId);
-    
+    MerchantPos selectBySnCodeAndInnerCode(@Param("snCode") String snCode, @Param("innerCode") String innerCode, @Param("channelId") Integer channelId);
+
     /**
      * selectByTerminalCode:(根据终端号查询SN号 )
      *
@@ -130,5 +134,7 @@ public interface MerchantPosDao {
      * @author tangliang
      * @date   2017年11月23日 下午1:53:39
      */
-    MerchantPos selectByTerminalCode(@Param("terminalCode")String terminalCode);
+    MerchantPos selectByTerminalCode(@Param("terminalCode") String terminalCode);
+
+    MerchantPos selectOneByTerminalCodeChannelType(@Param("termId") String termId, @Param("channelType") String channelType);
 }
