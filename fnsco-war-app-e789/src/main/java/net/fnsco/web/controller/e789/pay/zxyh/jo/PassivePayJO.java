2b029f56ba55710a841b80e2c2e8a3665c81b746
icy-dust@qq.com <12345678>
2017-12-01 17:31:58 +0800

    优化

--- fnsco-war-app-e789/src/main/java/net/fnsco/web/controller/e789/pay/zxyh/jo/PassivePayJO.java
+++ fnsco-war-app-e789/src/main/java/net/fnsco/web/controller/e789/pay/zxyh/jo/PassivePayJO.java
@@ -1,5 +1,6 @@
 package net.fnsco.web.controller.e789.pay.zxyh.jo;
 
+import io.swagger.annotations.ApiModelProperty;
 import net.fnsco.core.base.JO;
 
 /**
@@ -12,13 +13,17 @@ public class PassivePayJO extends JO {
 
 	private static final long serialVersionUID = 1L;
 
+	@ApiModelProperty(value="内部商户号")
 	private String innerCode;// 内部商户号
+	@ApiModelProperty(value="商品描述")
 	private String body;// 商品描述
+	@ApiModelProperty(value="交易金额")
 	private String amt;// 交易金额
-	private String discamt;// 支付宝不可优惠金额(支付宝交易必填字段)
+	@ApiModelProperty(value="授权码(付款码)")
 	private String authid;// 授权码，扫码支付授权码
+	@ApiModelProperty(value="签名")
 	private String signAture;// 签名
-
+	@ApiModelProperty(value="原支付订单号(用于交易状态查询)")
 	private String orgOrderId;// 原支付商户订单号(原交易返回，此处便于交易状态查询交易)
 
 	public String getInnerCode() {
@@ -43,14 +48,6 @@ public class PassivePayJO extends JO {
 
 	public void setAmt(String amt) {
 		this.amt = amt;
-	}
-
-	public String getDiscamt() {
-		return discamt;
-	}
-
-	public void setDiscamt(String discamt) {
-		this.discamt = discamt;
 	}
 
 	public String getAuthid() {
