830184b98ab7f6f10fc079991b35db221dc05a23
icy-dust@qq.com <12345678>
2017-12-05 10:06:32 +0800

    优化

--- fnsco-war-app-e789/src/main/java/net/fnsco/web/controller/e789/pay/zxyh/PassivePayController.java
+++ fnsco-war-app-e789/src/main/java/net/fnsco/web/controller/e789/pay/zxyh/PassivePayController.java
@@ -6,12 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson.JSON;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import net.fnsco.core.base.BaseController;
 import net.fnsco.trading.service.pay.channel.zxyh.PaymentService;
 import net.fnsco.trading.service.pay.channel.zxyh.dto.PassivePayDTO;
 import net.fnsco.web.controller.e789.pay.zxyh.jo.PassivePayJO;
+import net.fnsco.web.controller.e789.pay.zxyh.jo.PassiveResultQueryJO;
 
 @RestController
 @RequestMapping(value="/app/zxyh/PassivePay", method=RequestMethod.POST)
@@ -28,39 +31,41 @@ public class PassivePayController extends BaseController{
 	 */
 	@RequestMapping("/pay")
 	@ApiOperation(value="中信银行支付宝/微信被扫支付交易接口url")
-	public String ZxyhPassivePay(@RequestBody PassivePayJO zxyhPassivePayJO){
+	public String ZxyhPassivePay(@RequestBody PassivePayJO passivePayJO){
 		
 		//对接收的报文进行处理
+		String str = JSON.toJSONString(passivePayJO);
+		
 		
 		//签名校验
 		
 		PassivePayDTO passivePayDTO = new PassivePayDTO();
-		passivePayDTO.setStdtranamt(zxyhPassivePayJO.getAmt());//交易金额
-		passivePayDTO.setStd400memo(zxyhPassivePayJO.getBody());//商品描述
-		passivePayDTO.setStdauthid(zxyhPassivePayJO.getAuthid());//授权码
-		passivePayDTO.setSignAture(zxyhPassivePayJO.getSignAture());//签名
+		passivePayDTO.setStdtranamt(passivePayJO.getAmt());//交易金额
+		passivePayDTO.setStd400memo(passivePayJO.getBody());//商品描述
+		passivePayDTO.setStdauthid(passivePayJO.getAuthid());//授权码
+		passivePayDTO.setSignAture(passivePayJO.getSignAture());//签名
 
 		//调用service处理(值补全和存储以及发送请求给中信银行),并返回应答
-		return PaymentService.PassivePay(zxyhPassivePayJO.getInnerCode(), passivePayDTO);
+		return PaymentService.PassivePay(passivePayJO.getInnerCode(), passivePayDTO);
 	}
 
 	/**
-	 * 支付结果查询(被扫之后，需要客户在手机上面手输密码点击确认才能支付成功，此时商户这边并不知道是否支付成功，需要再次调用查询接口来查询交易结果，商户刷新交易界面)
+	 * 支付结果查询(被扫之后，有的需要客户在手机上面手输密码点击确认才能支付成功，此时商户这边并不知道是否支付成功，需要再次调用查询接口来查询交易结果，商户刷新交易界面)
 	 * @param passivePayDTO:带查询的必要元素对象(JSON字串)
 	 * @return
 	 */
 	@RequestMapping("/queryPayResult")
 	@ApiOperation(value="中信银行支付宝/微信支付结果查询接口url")
-	public String ZxyhPassivePayResult(@RequestBody PassivePayJO zxyhPassivePayJO) {
+	public String ZxyhPassivePayResult(@RequestBody PassiveResultQueryJO passiveResultQueryJO) {
 
 		// 对接收的报文进行处理
 
 		// 签名校验
 
 		PassivePayDTO passivePayDTO = new PassivePayDTO();
-		passivePayDTO.setOrgorderid((zxyhPassivePayJO.getOrgOrderId()));// 原商户订单号
-		passivePayDTO.setSignAture(zxyhPassivePayJO.getSignAture());// 签名
+		passivePayDTO.setOrgorderid((passiveResultQueryJO.getOrgOrderId()));// 原商户订单号
+		passivePayDTO.setSignAture(passiveResultQueryJO.getSignAture());// 签名
 
-		return PaymentService.PassivePayResult(zxyhPassivePayJO.getOrgOrderId());
+		return PaymentService.PassivePayResult(passiveResultQueryJO.getOrgOrderId());
 	}
 }
