3be4ea8129dffd52894b926d166f3489d0265958
liang.tang <liang.tang@zheft.cn>
2017-12-06 14:03:50 +0800

    提交

--- fnsco-war-app-e789/src/main/java/net/fnsco/web/controller/e789/tradedata/TradedataE789Controller.java
+++ fnsco-war-app-e789/src/main/java/net/fnsco/web/controller/e789/tradedata/TradedataE789Controller.java
@@ -9,7 +9,10 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import net.fnsco.core.base.BaseController;
 import net.fnsco.core.base.ResultDTO;
-import net.fnsco.web.controller.e789.jo.CommonJO;
+import net.fnsco.web.controller.e789.jo.TradeDataDetailJO;
+import net.fnsco.web.controller.e789.jo.TradeDataJO;
+import net.fnsco.web.controller.e789.vo.TradeDataDetailVO;
+import net.fnsco.web.controller.e789.vo.TradeDataVO;
 
 /**
  * @desc 流水查询控制器
@@ -26,7 +29,7 @@ public class TradedataE789Controller extends BaseController {
 	/**
 	 * queryTradeDataList:(查询流水列表)
 	 *
-	 * @param  @param commonJO
+	 * @param  @param tradeDataJO
 	 * @param  @return    设定文件
 	 * @return ResultDTO<Object>    DOM对象
 	 * @author tangliang
@@ -34,7 +37,7 @@ public class TradedataE789Controller extends BaseController {
 	 */
 	@RequestMapping(value = "/query")
 	@ApiOperation(value = "查询交易流水列表")
-	public ResultDTO<Object> queryTradeDataList(@RequestBody CommonJO commonJO){
+	public ResultDTO<TradeDataVO> queryTradeDataList(@RequestBody TradeDataJO tradeDataJO){
 		return null;
 	}
 	
@@ -42,15 +45,15 @@ public class TradedataE789Controller extends BaseController {
 	/**
 	 * queryTradeDataDetail:(查询交易流水详情)
 	 *
-	 * @param  @param commonJO
+	 * @param  @param tradeDataDetailJO
 	 * @param  @return    设定文件
-	 * @return ResultDTO<Object>    DOM对象
+	 * @return ResultDTO<TradeDataDetailVO>    DOM对象
 	 * @author tangliang
 	 * @date   2017年12月5日 下午4:07:33
 	 */
 	@RequestMapping(value = "/queryDetail")
 	@ApiOperation(value = "查询交易流水详情")
-	public ResultDTO<Object> queryTradeDataDetail(@RequestBody CommonJO commonJO){
+	public ResultDTO<TradeDataDetailVO> queryTradeDataDetail(@RequestBody TradeDataDetailJO tradeDataDetailJO){
 		return null;
 	}
 	
