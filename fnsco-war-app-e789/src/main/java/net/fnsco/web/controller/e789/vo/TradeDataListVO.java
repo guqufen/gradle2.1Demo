ac83443c957f079b99974825518785accfda5c78
liang.tang <liang.tang@zheft.cn>
2017-12-08 10:23:02 +0800

    提交代码

--- fnsco-war-app-e789/src/main/java/net/fnsco/web/controller/e789/vo/TradeDataListVO.java
+++ fnsco-war-app-e789/src/main/java/net/fnsco/web/controller/e789/vo/TradeDataListVO.java
@@ -24,7 +24,7 @@ public class TradeDataListVO extends VO {
 	/**
 	 * 交易状态
 	 */
-	@ApiModelProperty(value="交易状态",example="交易状态")
+	@ApiModelProperty(value="交易状态1000处理中1001成功1002失败1003已退货",example="交易状态1000处理中1001成功1002失败1003已退货")
 	private String tradeStatus;
 	
 	/**
@@ -42,6 +42,66 @@ public class TradeDataListVO extends VO {
 	@ApiModelProperty(value="流水ID",example="流水ID，可以根据该ID 查询出详情")
 	private String traId;
 	
+	@ApiModelProperty(value="交易子类型00刷卡01微信02支付宝03聚惠分",example="交易子类型00刷卡01微信02支付宝03聚惠分")
+	private String paySubType;
+	
+	@ApiModelProperty(value="订单号",example="订单号")
+	private String orderNo;
+	
+	/**
+	 * paySubType
+	 *
+	 * @return  the paySubType
+	 * @since   CodingExample Ver 1.0
+	*/
+	
+	public String getPaySubType() {
+		return paySubType;
+	}
+
+	/**
+	 * paySubType
+	 *
+	 * @param   paySubType    the paySubType to set
+	 * @since   CodingExample Ver 1.0
+	 */
+	
+	public void setPaySubType(String paySubType) {
+		this.paySubType = paySubType;
+	}
+
+	/**
+	 * orderNo
+	 *
+	 * @return  the orderNo
+	 * @since   CodingExample Ver 1.0
+	*/
+	
+	public String getOrderNo() {
+		return orderNo;
+	}
+
+	/**
+	 * orderNo
+	 *
+	 * @param   orderNo    the orderNo to set
+	 * @since   CodingExample Ver 1.0
+	 */
+	
+	public void setOrderNo(String orderNo) {
+		this.orderNo = orderNo;
+	}
+
+	/**
+	 * serialversionuid
+	 *
+	 * @return  the serialversionuid
+	 * @since   CodingExample Ver 1.0
+	*/
+	
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 
 	/**
 	 * traId
