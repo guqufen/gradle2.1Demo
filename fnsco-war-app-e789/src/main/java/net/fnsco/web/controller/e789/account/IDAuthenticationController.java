3074abb41d56065d88e86e1468ab019a01f85764
hjt <jinting.he@zheft.cn>
2017-12-12 14:47:45 +0800

    更新

--- fnsco-war-app-e789/src/main/java/net/fnsco/web/controller/e789/account/IDAuthenticationController.java
+++ fnsco-war-app-e789/src/main/java/net/fnsco/web/controller/e789/account/IDAuthenticationController.java
@@ -1,14 +1,30 @@
 package net.fnsco.web.controller.e789.account;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Calendar;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.beust.jcommander.internal.Maps;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import net.fnsco.core.base.BaseController;
 import net.fnsco.core.base.ResultDTO;
+import net.fnsco.core.utils.OssLoaclUtil;
+import net.fnsco.order.api.appuser.AppUserService;
+import net.fnsco.order.api.constant.ApiConstant;
+import net.fnsco.order.api.dto.AppUserDTO;
 import net.fnsco.web.controller.e789.jo.AccountBalanceJO;
 import net.fnsco.web.controller.e789.vo.AccountBalanceVO;
 
@@ -23,12 +39,82 @@ import net.fnsco.web.controller.e789.vo.AccountBalanceVO;
 @RequestMapping(value = "/app2c/id", method = RequestMethod.POST)
 @Api(value = "/app2c/id", tags = { "我的-身份证认证相关功能接口" })
 public class IDAuthenticationController extends BaseController {
-	
-	
+	 @Autowired
+	 private AppUserService        appUserService;
+	 @Autowired
+	 private Environment           env;
 	@RequestMapping(value = "/auth")
     @ApiOperation(value = "个人信息-身份证认证接口")
     public ResultDTO<AccountBalanceVO> idAuth(@RequestBody AccountBalanceJO accountBalanceJO) {
- 		
+		MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+        Map<String, MultipartFile> fileMap = multipartRequest.getFileMap();
+        for (Map.Entry<String, MultipartFile> entity : fileMap.entrySet()) {
+        	Integer userId = accountBalanceJO.getUserId();
+    		if (userId == null) {
+    			return ResultDTO.fail(ApiConstant.E_USER_ID_NULL);
+    		}
+            // 上传文件原名
+            MultipartFile file = entity.getValue();
+            String fileName = file.getOriginalFilename();
+            String line = System.getProperty("file.separator");// 文件分割符
+            // 保存文件的路径
+            String prefix = fileName.substring(fileName.lastIndexOf(".") + 1);
+            // 数据库存的路径
+            Calendar cal = Calendar.getInstance();
+            int year = cal.get(Calendar.YEAR);
+            int month = cal.get(Calendar.MONTH) + 1;
+            String stry = this.env.getProperty("fileUpload.url") + line + year;// +"\\"+month+"\\";
+            File yearPath = new File(stry);
+            // 如果文件夹不存在则创建
+            if (!yearPath.exists()) {
+                logger.info("年份目录不存在");
+                yearPath.mkdirs();
+            } else {
+                logger.info("年份目录已存在");
+            }
+
+            String strm = this.env.getProperty("fileUpload.url") + line + year + line + month + line;
+            File monthPath = new File(strm);
+            if (!monthPath.exists()) {
+                logger.info("月份目录不存在");
+                monthPath.mkdirs();
+            } else {
+                logger.info("月份目录已存在");
+            }
+
+            String yearMonthPath = year + line + month + line;
+            String newFileName = System.currentTimeMillis() + "." + prefix;
+            String fileKey = year + "/" + month + "/" + newFileName;
+            String filepath = yearMonthPath + newFileName;
+
+            String fileURL = this.env.getProperty("fileUpload.url") + line + filepath;
+
+            if (!file.isEmpty()) {
+                try {
+                    byte[] bytes = file.getBytes();
+                    BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(fileURL));
+                    stream.write(bytes);
+                    stream.close();
+                    //上传阿里云OSS文件服务器
+                    OssLoaclUtil.uploadFile(fileURL, fileKey);
+                    String newUrl = OssLoaclUtil.getHeadBucketName() + "^" + fileKey;
+                    AppUserDTO appUserDto = new AppUserDTO();
+                    appUserDto.setUserId(userId);
+                    appUserDto.setHeadImagePath(newUrl);
+                    appUserService.modifyInfo(appUserDto);
+                    String imageUrl = OssLoaclUtil.getForeverFileUrl(OssLoaclUtil.getHeadBucketName(), fileKey);
+                    Map<String, String> datas = Maps.newHashMap();
+                    datas.put("headImageUrl", imageUrl);
+                    return ResultDTO.success(datas);
+                } catch (Exception e) {
+                    logger.error(fileName + "上传失败！" + e);
+                    throw new RuntimeException();
+                }
+            } else {
+                logger.error(fileName + "上传失败");
+                throw new RuntimeException();
+            }
+        }
         return success(null);
     }
 }
