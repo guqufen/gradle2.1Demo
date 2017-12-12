3f504b7fab7d6abf7c188a6a752d93295e16b0ff
lbh <binghui.li@zheft.cn>
2017-12-12 10:47:33 +0800

    0

--- fnsco-war-car/src/main/java/net/fnsco/web/controller/open/common/SendMessageController.java
+++ fnsco-war-car/src/main/java/net/fnsco/web/controller/open/common/SendMessageController.java
@@ -1,19 +1,24 @@
 package net.fnsco.web.controller.open.common;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import net.fnsco.core.base.ResultDTO;
 import net.fnsco.core.utils.MessageUtils;
 
 @RestController
-@RequestMapping(value = "/h5/send", method = RequestMethod.POST)
-@Api(value = "/h5/send", tags = { "发送验证码" })
+@RequestMapping(value = "/h5", method = RequestMethod.POST)
+@Api(value = "/h5", tags = { "发送验证码" })
 public class SendMessageController {
 	
-	public ResultDTO sendMessage(String mobile){
+	
+	@RequestMapping(value = "/sendMessage")
+	@ApiOperation(value = "发送验证码")
+	public ResultDTO sendMessage(@RequestBody String mobile){
 		String deviceId = "fns";
 		MessageUtils mUtils = new MessageUtils();
 		ResultDTO result = mUtils.sendValidateCode(deviceId, mobile);
