87415ada2b8a0f61002ae23d97abf28598e801fc
lbh <binghui.li@zheft.cn>
2017-12-12 18:04:23 +0800

    0

--- fnsco-war-car/src/main/java/net/fnsco/web/controller/jo/LoanJO.java
+++ fnsco-war-car/src/main/java/net/fnsco/web/controller/jo/LoanJO.java
@@ -4,29 +4,32 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 import io.swagger.annotations.ApiModelProperty;
+import net.fnsco.core.base.JO;
 
-public class LoanJO {
+public class LoanJO extends JO{
 
-	@ApiModelProperty(value = "name", name = "name", example = "购车人")
+	
+	
+	@ApiModelProperty(value = "购车人",  example = "购车人")
 	private String name;
-	@ApiModelProperty(value = "mobile", name = "mobile", example = "手机号")
+	@ApiModelProperty(value = "手机号", example = "手机号")
 	private String mobile;
 
 	/**
 	 * 所在城市
 	 */
-	@ApiModelProperty(value = "cityId", name = "cityId", example = "所在城市")
+	@ApiModelProperty(value = "所在城市", example = "所在城市")
 	private Integer cityId;
 
 	/**
 	 * 贷款金额
 	 */
-	@ApiModelProperty(value = "amount", name = "amount", example = "贷款金额")
+	@ApiModelProperty(value = "贷款金额", example = "贷款金额")
 	private BigDecimal amount;
 	/**
 	 * 推荐码
 	 */
-	@ApiModelProperty(value = "suggestCode", name = "suggestCode", example = "推荐码")
+	@ApiModelProperty(value = "验证码",  example = "验证码")
 	private Integer suggestCode;
 	
 	
@@ -34,23 +37,23 @@ public class LoanJO {
 	/**
 	 * 验证码
 	 */
-	@ApiModelProperty(value = "verCode", name = "verCode", example = "验证码")
+	@ApiModelProperty(value = "验证码",  example = "验证码")
 	private String verCode;
 	/**
 	 * 车品牌
 	 */
-	@ApiModelProperty(value = "carTypeId", name = "carTypeId", example = "车品牌")
+	@ApiModelProperty(value = "车品牌",  example = "车品牌")
 	private Integer carTypeId;
 	/**
 	 * 车型号
 	 */
-	@ApiModelProperty(value = "carModel", name = "carModel", example = "车型号")
+	@ApiModelProperty(value = "车型号",  example = "车型号")
 	private String carModel;
 
-	@ApiModelProperty(value = "fileIds", name = "fileIds", example = "文件id")
+	@ApiModelProperty(value = "文件id", example = "文件id")
 	private String fileIds;
 	
-	@ApiModelProperty(value = "type", name = "type", example = "申请类型")
+	@ApiModelProperty(value = "申请类型", example = "申请类型")
 	private String type;
 	/**
 	 * @return the type
