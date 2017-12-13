3f906091e251777599ded4178593a2c4dcbfc498
lbh <binghui.li@zheft.cn>
2017-12-13 10:53:36 +0800

    0

--- fnsco-war-car/src/main/java/net/fnsco/web/controller/open/LoanApplyController.java
+++ fnsco-war-car/src/main/java/net/fnsco/web/controller/open/LoanApplyController.java
@@ -86,8 +86,8 @@ public class LoanApplyController extends BaseController {
 		orderLoan.setCityId(jo.getCityId());
 		orderLoan.setAmount(jo.getAmount());
 		orderLoan.setSuggestCode(jo.getSuggestCode());
-		orderLoan.setCarSubTypeId(jo.getCarSubTypeId());// 车品牌id
-		orderLoan.setCarModel(jo.getMobile());// 车型号
+		orderLoan.setCarTypeId(jo.getCarTypeId());// 车品牌id
+		orderLoan.setCarSubTypeId(jo.getCarSubTypeId());// 车型号id
 
 		String fileIds = jo.getFileIds();// 上传的图片id
 		ResultDTO<Object> result = orderLoanService.addJo(orderLoan, customer, fileIds);
