c32fae557bb7d02770d497858393d892a5adea51
lbh <binghui.li@zheft.cn>
2017-12-27 13:22:37 +0800

    付款回调修改

--- fnsco-service-trading/src/main/java/net/fnsco/trading/service/order/dao/helper/TradeOrderProvider.java
+++ fnsco-service-trading/src/main/java/net/fnsco/trading/service/order/dao/helper/TradeOrderProvider.java
@@ -17,6 +17,99 @@ public class TradeOrderProvider {
 
 	private static final String TABLE_NAME = "t_trade_order";
 
+	
+	public String updateByOrderId(Map<String, Object> params) {
+		TradeOrderDO tradeOrder = (TradeOrderDO) params.get("tradeOrder");
+		return new SQL() {
+			{
+				UPDATE(TABLE_NAME);
+//				if (StringUtils.isNotBlank(tradeOrder.getOrderNo())) {
+//					SET("order_no=#{tradeOrder.orderNo}");
+//				}
+				if (StringUtils.isNotBlank(tradeOrder.getPayOrderNo())) {
+					SET("pay_order_no=#{tradeOrder.payOrderNo}");
+				}
+				if (tradeOrder.getTxnAmount() != null) {
+					SET("txn_amount=#{tradeOrder.txnAmount}");
+				}
+				if (tradeOrder.getInstallmentNum() != null) {
+					SET("installment_num=#{tradeOrder.installmentNum}");
+				}
+				if (StringUtils.isNotBlank(tradeOrder.getRespCode())) {
+					SET("resp_code=#{tradeOrder.respCode}");
+				}
+				if (StringUtils.isNotBlank(tradeOrder.getRespMsg())) {
+					SET("resp_msg=#{tradeOrder.respMsg}");
+				}
+				if (StringUtils.isNotBlank(tradeOrder.getEntityInnerCode())) {
+					SET("entity_inner_code=#{tradeOrder.entityInnerCode}");
+				}
+				if (StringUtils.isNotBlank(tradeOrder.getChannelMerId())) {
+					SET("channel_mer_id=#{tradeOrder.channelMerId}");
+				}
+				if (StringUtils.isNotBlank(tradeOrder.getChannelType())) {
+					SET("channel_type=#{tradeOrder.channelType}");
+				}
+				if (tradeOrder.getCompleteTime() != null) {
+					SET("complete_time=#{tradeOrder.completeTime}");
+				}
+				if (tradeOrder.getOrderCeateTime() != null) {
+					SET("order_ceate_time=#{tradeOrder.orderCeateTime}");
+				}
+				if (tradeOrder.getTxnType() != null) {
+					SET("txn_type=#{tradeOrder.txnType}");
+				}
+				if (tradeOrder.getTxnSubType() != null) {
+					SET("txn_sub_type=#{tradeOrder.txnSubType}");
+				}
+				if (StringUtils.isNotBlank(tradeOrder.getPayType())) {
+					SET("pay_type=#{tradeOrder.payType}");
+				}
+				if (StringUtils.isNotBlank(tradeOrder.getPaySubType())) {
+					SET("pay_sub_type=#{tradeOrder.paySubType}");
+				}
+				if (tradeOrder.getSettleAmount() != null) {
+					SET("settle_amount=#{tradeOrder.settleAmount}");
+				}
+				if (tradeOrder.getSettleDate() != null) {
+					SET("settle_date=#{tradeOrder.settleDate}");
+				}
+				if (tradeOrder.getSettleStatus() != null) {
+					SET("settle_status=#{tradeOrder.settleStatus}");
+				}
+				if (StringUtils.isNotBlank(tradeOrder.getCreateUserId())) {
+					SET("create_user_id=#{tradeOrder.createUserId}");
+				}
+				if (tradeOrder.getCreateTime() != null) {
+					SET("create_time=#{tradeOrder.createTime}");
+				}
+				if (tradeOrder.getSyncStatus() != null) {
+					SET("sync_status=#{tradeOrder.syncStatus}");
+				}
+				if (StringUtils.isNotBlank(tradeOrder.getInnerCode())) {
+					SET("inner_code=#{tradeOrder.innerCode}");
+				}
+				if (tradeOrder.getHandleNum() != null) {
+					SET("handle_num=#{tradeOrder.handleNum}");
+				}
+				if (tradeOrder.getOrderAmount() != null) {
+					SET("order_amount=#{tradeOrder.orderAmount}");
+				}
+				if (tradeOrder.getEachMoney() != null) {
+					SET("each_money=#{tradeOrder.eachMoney}");
+				}
+				if (StringUtils.isNotBlank(tradeOrder.getCardHolderRate())) {
+					SET("card_holder_rate=#{tradeOrder.cardHolderRate}");
+				}
+				if (StringUtils.isNotBlank(tradeOrder.getPayMedium())) {
+					SET("pay_medium=#{tradeOrder.payMedium}");
+				}
+				WHERE("order_no = #{tradeOrder.orderNo}");
+			}
+		}.toString();
+	}
+	
+	
 	public String updateOnlyFail(Map<String, Object> params) {
 		TradeOrderDO tradeOrder = (TradeOrderDO) params.get("tradeOrder");
 		return new SQL() {
--- fnsco-war-app-e789/src/main/java/net/fnsco/web/controller/e789/pay/zxyh/ScannedTradeController.java
+++ fnsco-war-app-e789/src/main/java/net/fnsco/web/controller/e789/pay/zxyh/ScannedTradeController.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,14 +13,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.google.common.base.Strings;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import net.fnsco.bigdata.api.dto.TradeWeChatCallBackDTO;
-import net.fnsco.bigdata.api.trade.TradeDataService;
 import net.fnsco.core.base.BaseController;
 import net.fnsco.core.base.ResultDTO;
+import net.fnsco.order.api.dto.TradeReportDTO;
+import net.fnsco.trading.service.order.entity.TradeOrderDO;
 import net.fnsco.trading.service.pay.channel.pfyh.PFOrderPaymentService;
 import net.fnsco.trading.service.pay.channel.zxyh.PaymentService;
 import net.fnsco.web.controller.e789.jo.GenerateQRJO;
@@ -40,7 +43,6 @@ public class ScannedTradeController extends BaseController {
 	private PaymentService zxyhPaymentService;
 	@Autowired
 	private PFOrderPaymentService pfOrderPaymentService;
-
 
 	@RequestMapping(value = "/getQRUrl")
 	@ApiOperation(value = "收款-获取付款二维码url")
@@ -69,7 +71,7 @@ public class ScannedTradeController extends BaseController {
 			dto = zxyhPaymentService.generateQRCodeAliPay(userId, ip, txnAmt);
 		}
 		reqMap = dto.getData();
-		qrVo.setUrl((String) reqMap.get("codeUrl"));
+		qrVo.setUrl((String) reqMap.getOrDefault("codeUrl", null));
 		if ("0000".equals(reqMap.get("respCode"))) {
 			return ResultDTO.success(qrVo);
 		} else {
@@ -80,17 +82,18 @@ public class ScannedTradeController extends BaseController {
 	}
 
 	/**
-	 * 微信扫码接口回调
-	 * callBack:(这里用一句话描述这个方法的作用)
+	 * 微信扫码接口回调 更新交易状态 callBack:(这里用一句话描述这个方法的作用)
 	 *
-	 * @param  @param respStr    设定文件
-	 * @return void    DOM对象
-	 * @auth binghui.li 
-	 * @since  CodingExample　Ver 1.1
+	 * @param @param
+	 *            respStr 设定文件
+	 * @return void DOM对象
+	 * @auth binghui.li
+	 * @since CodingExample Ver 1.1
 	 */
 	@RequestMapping(value = "/callBack")
 	@ApiOperation(value = "收款-扫码付款回调函数")
-	public void callBack(String respStr) {
+	public void callBack(@RequestBody String respStr) {
+		// 解析返回报文
 		assert respStr.startsWith("sendData=");
 		String respB64Str = respStr.substring(9);
 		// base64解码,并对一些特殊字符进行置换
@@ -101,12 +104,14 @@ public class ScannedTradeController extends BaseController {
 		} catch (UnsupportedEncodingException e) {
 			throw new RuntimeException(e);
 		}
-		// 解析json
-		TradeWeChatCallBackDTO tradeWeChatCallBackDTO = JSON.parseObject(respJsStr, TradeWeChatCallBackDTO.class);
-		if(tradeWeChatCallBackDTO != null){
-//			tradeDataService.updateByOrderId(tradeWeChatCallBackDTO);
+		JSONObject jsonObj = JSONObject.parseObject(respJsStr);
+		String txnSubType = jsonObj.getString("txnSubType");
+		logger.warn("中信被扫回调函数返回值txnSubType="+"["+ txnSubType +"]");
+		if(StringUtils.equals("010130", txnSubType)){//微信回调
+			zxyhPaymentService.weChatCallBack(respJsStr);
+		}else if(StringUtils.equals("010302", txnSubType)){//微信回调
+			zxyhPaymentService.aliCallBack(respJsStr);
 		}
-		
 	}
 
 }
