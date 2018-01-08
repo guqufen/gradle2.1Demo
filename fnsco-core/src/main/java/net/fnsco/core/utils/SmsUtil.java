87e47c18334508bbfee30778e0d68b7838da71b2
lbh <binghui.li@zheft.cn>
2018-01-08 10:43:09 +0800

    更换短信模e789

--- fnsco-core/src/main/java/net/fnsco/core/utils/SmsUtil.java
+++ fnsco-core/src/main/java/net/fnsco/core/utils/SmsUtil.java
@@ -79,7 +79,7 @@ public class SmsUtil {
         long tpl_id = 2139788;
         //设置对应的模板变量值
 
-        String tpl_value = URLEncoder.encode("#code#", ENCODING) + "=" + URLEncoder.encode(code, ENCODING) + "&" + URLEncoder.encode("#company#", ENCODING) + "=" + URLEncoder.encode("E789", ENCODING);
+        String tpl_value = URLEncoder.encode("#code#", ENCODING) + "=" + URLEncoder.encode(code, ENCODING);// + "&" + URLEncoder.encode("#company#", ENCODING) + "=" + URLEncoder.encode("e789", ENCODING);
 
         String result = tplSendSms(apikey, tpl_id, tpl_value, mobile);
 
