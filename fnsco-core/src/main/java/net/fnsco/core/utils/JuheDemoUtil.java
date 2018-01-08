f43ecae64da95c718dac01f017f96177a9aec430
hjt <jinting.he@zheft.cn>
2018-01-08 12:03:40 +0800

    hjt身份证验证

--- fnsco-core/src/main/java/net/fnsco/core/utils/JuheDemoUtil.java
+++ fnsco-core/src/main/java/net/fnsco/core/utils/JuheDemoUtil.java
@@ -1,8 +1,15 @@
 package net.fnsco.core.utils;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -35,6 +42,11 @@ public class JuheDemoUtil {
     
     //身份证实名认证的http地址
     private static String  URI_ID_CARD      = "http://op.juhe.cn/idcard/query";
+    
+    public static final String DEF_CHATSET = "UTF-8";
+    public static final int DEF_CONN_TIMEOUT = 30000;
+    public static final int DEF_READ_TIMEOUT = 30000;
+    public static String userAgent =  "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36";
     /**
      * 图片识别
      * @param firstKey
@@ -79,9 +91,22 @@ public class JuheDemoUtil {
     	Map<String,String> params = Maps.newHashMap();
     	params.put("key", CARD_APPKEY);
     	params.put("idcard", idcard );
+    	/*try {
+    		realname = URLEncoder.encode(realname,"UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			logger.info("URLEncoder编码失败");
+			e.printStackTrace();
+		}*/
     	params.put("realname", realname);
-    	String resule = HttpClientUtil.doPost(URI_ID_CARD, params);
-    	logger.info("resule返回值:"+resule);
+    	String resule = null;
+		try {
+			resule = net(URI_ID_CARD, params,"GET");
+		} catch (Exception e) {
+			logger.info("实名认证验证失败");
+			e.printStackTrace();
+		}
+    	//String resule = HttpUtils.get(URI_ID_CARD, params);
+    	//String resule = HttpClientUtil.doPost(URI_ID_CARD, params);
     	 JSONObject json = JSONObject.parseObject(resule);
     	 int errorCode = json.getInteger("error_code");
     	 String reason = json.getString("reason");
@@ -124,4 +149,77 @@ public class JuheDemoUtil {
         BASE64Encoder encoder = new BASE64Encoder();
         return encoder.encode(data);
     }
+    
+    
+    /**
+    *
+    * @param strUrl 请求地址
+    * @param params 请求参数
+    * @param method 请求方法
+    * @return  网络请求字符串
+    * @throws Exception
+    */
+   public static String net(String strUrl, Map params,String method) throws Exception {
+       HttpURLConnection conn = null;
+       BufferedReader reader = null;
+       String rs = null;
+       try {
+           StringBuffer sb = new StringBuffer();
+           if(method==null || method.equals("GET")){
+               strUrl = strUrl+"?"+urlencode(params);
+           }
+           URL url = new URL(strUrl);
+           conn = (HttpURLConnection) url.openConnection();
+           if(method==null || method.equals("GET")){
+               conn.setRequestMethod("GET");
+           }else{
+               conn.setRequestMethod("POST");
+               conn.setDoOutput(true);
+           }
+           conn.setRequestProperty("User-agent", userAgent);
+           conn.setUseCaches(false);
+           conn.setConnectTimeout(DEF_CONN_TIMEOUT);
+           conn.setReadTimeout(DEF_READ_TIMEOUT);
+           conn.setInstanceFollowRedirects(false);
+           conn.connect();
+           if (params!= null && method.equals("POST")) {
+               try {
+                   DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+                       out.writeBytes(urlencode(params));
+               } catch (Exception e) {
+                   // TODO: handle exception
+               }
+           }
+           InputStream is = conn.getInputStream();
+           reader = new BufferedReader(new InputStreamReader(is, DEF_CHATSET));
+           String strRead = null;
+           while ((strRead = reader.readLine()) != null) {
+               sb.append(strRead);
+           }
+           rs = sb.toString();
+       } catch (IOException e) {
+           e.printStackTrace();
+       } finally {
+           if (reader != null) {
+               reader.close();
+           }
+           if (conn != null) {
+               conn.disconnect();
+           }
+       }
+       return rs;
+   }
+
+   //将map型转为请求参数型
+   public static String urlencode(Map<String,Object>data) {
+       StringBuilder sb = new StringBuilder();
+       for (Map.Entry i : data.entrySet()) {
+           try {
+               sb.append(i.getKey()).append("=").append(URLEncoder.encode(i.getValue()+"","UTF-8")).append("&");
+           } catch (UnsupportedEncodingException e) {
+               e.printStackTrace();
+           }
+       }
+       return sb.toString();
+   }
 }
