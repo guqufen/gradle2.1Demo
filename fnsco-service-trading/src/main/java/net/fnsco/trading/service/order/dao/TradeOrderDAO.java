a35d955d59cde6edc7a5ee7c55633830a854fb60
lbh <binghui.li@zheft.cn>
2018-01-13 16:56:02 +0800

    0

--- fnsco-service-trading/src/main/java/net/fnsco/trading/service/order/dao/TradeOrderDAO.java
+++ fnsco-service-trading/src/main/java/net/fnsco/trading/service/order/dao/TradeOrderDAO.java
@@ -159,6 +159,6 @@ public interface TradeOrderDAO {
     @UpdateProvider(type = TradeOrderProvider.class, method = "updateByOrderId")
 	public int updateByOrderId(TradeOrderDO tradeOrderDO);
 
-    @Select("SELECT STATUS FROM m_merchant_core WHERE inner_code IN ( SELECT inner_code FROM m_merchant_core_entity_ref WHERE entity_inner_code IN (SELECT ue.entity_inner_code FROM u_app_user uu,u_app_user_merchant_entity ue WHERE uu.id = ue.app_user_id 	AND uu.id = #{userId}))")
+    @Select("SELECT mc.channel_type,tt.inner_code FROM 	m_merchant_channel mc,	(SELECT inner_code	FROM m_merchant_core_entity_ref WHERE entity_inner_code IN (	SELECT ue.entity_inner_code FROM u_app_user uu,	u_app_user_merchant_entity ue WHERE uu.id = ue.app_user_id AND uu.id = #{userId} ) ) tt WHERE mc.inner_code = tt.inner_code AND channel_type = '05' ORDER BY mc.modify_time DESC LIMIT 1")
 	public Integer getAddStatus(@Param("userId")Integer userId);
 }
