d36ea3e2f4d71472d2553ed205a58b87f2dd4c31
liang.tang <liang.tang@zheft.cn>
2018-01-16 17:39:49 +0800

    去除多余宝

--- fnsco-service-trading/src/main/java/net/fnsco/trading/service/pay/channel/ebank/EbankService.java
+++ fnsco-service-trading/src/main/java/net/fnsco/trading/service/pay/channel/ebank/EbankService.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import org.mockito.exceptions.verification.NeverWantedButInvoked;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
