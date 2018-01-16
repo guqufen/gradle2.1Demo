3e46981a09f5e2c9e5d36f47d93d190db2d28298
yx <icy-dust@qq.com>
2018-01-16 11:05:05 +0800

    优化,e789平安银行代扣接口

--- fnsco-war-app-e789/src/main/java/net/fnsco/web/controller/e789/pay/ebank/jo/E4028JO.java
+++ fnsco-war-app-e789/src/main/java/net/fnsco/web/controller/e789/pay/ebank/jo/E4028JO.java
@@ -8,11 +8,6 @@ public class E4028JO extends JO {
 	private static final long serialVersionUID = 1L;
 	@ApiModelProperty(name = "付款人银行卡号", example = "付款人银行卡号")
 	private String oppAccNo;// 付款人帐号
-	/**
-	 * APP登录用户ID
-	 */
-	@ApiModelProperty(value = "APP登录用户ID", name = "userId", example = "")
-	private Integer userId;
 
 	public String getOppAccNo() {
 		return oppAccNo;
@@ -24,14 +19,6 @@ public class E4028JO extends JO {
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
-	}
-
-	public Integer getUserId() {
-		return userId;
-	}
-
-	public void setUserId(Integer userId) {
-		this.userId = userId;
 	}
 
 }
--- fnsco-war-app-e789/src/main/java/net/fnsco/web/controller/e789/pay/ebank/jo/E4033JO.java
+++ fnsco-war-app-e789/src/main/java/net/fnsco/web/controller/e789/pay/ebank/jo/E4033JO.java
@@ -8,11 +8,6 @@ public class E4033JO extends JO {
 	private static final long serialVersionUID = 1L;
 	@ApiModelProperty(name = "支付订单号", example = "支付订单号")
 	private String orderNo;
-	/**
-	 * APP登录用户ID
-	 */
-	@ApiModelProperty(value = "APP登录用户ID", name = "userId", example = "")
-	private Integer userId;
 
 	public String getOrderNo() {
 		return orderNo;
@@ -24,14 +19,6 @@ public class E4033JO extends JO {
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
-	}
-
-	public Integer getUserId() {
-		return userId;
-	}
-
-	public void setUserId(Integer userId) {
-		this.userId = userId;
 	}
 
 }
