e1f02bebb56d3b539dd968c7e71f87b7dd8dfb90
liang.tang <liang.tang@zheft.cn>
2018-01-23 17:14:23 +0800

    新增加邮件发送公共类

---
+++ fnsco-core/src/main/java/net/fnsco/core/mail/MailSendParams.java
@@ -0,0 +1,150 @@
+package net.fnsco.core.mail;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * @desc 发送邮件参数类
+ * @author   tangliang
+ * @version  0.0.1-SNAPSHOT
+ * @since    Ver 1.1
+ * @Date	 2018年1月23日 下午4:00:30
+ */
+
+public class MailSendParams {
+	
+	private String nick;//发送者称呼
+	
+	private String toTarget;//收件人
+	
+	private String toAddress;//发送目标邮箱地址
+	
+	private String sendText;//发送内容
+	
+	private String sendSubject;//发送邮件主题
+	
+	/**
+	 * sendSubject
+	 *
+	 * @return  the sendSubject
+	 * @since   CodingExample Ver 1.0
+	*/
+	
+	public String getSendSubject() {
+		return sendSubject;
+	}
+
+	/**
+	 * sendSubject
+	 *
+	 * @param   sendSubject    the sendSubject to set
+	 * @since   CodingExample Ver 1.0
+	 */
+	
+	public void setSendSubject(String sendSubject) {
+		this.sendSubject = sendSubject;
+	}
+
+	/**
+	 * nick
+	 *
+	 * @return  the nick
+	 * @since   CodingExample Ver 1.0
+	*/
+	
+	public String getNick() {
+		return nick;
+	}
+
+	/**
+	 * nick
+	 *
+	 * @param   nick    the nick to set
+	 * @since   CodingExample Ver 1.0
+	 */
+	
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+
+	/**
+	 * toTarget
+	 *
+	 * @return  the toTarget
+	 * @since   CodingExample Ver 1.0
+	*/
+	
+	public String getToTarget() {
+		return toTarget;
+	}
+
+	/**
+	 * toTarget
+	 *
+	 * @param   toTarget    the toTarget to set
+	 * @since   CodingExample Ver 1.0
+	 */
+	
+	public void setToTarget(String toTarget) {
+		this.toTarget = toTarget;
+	}
+
+	/**
+	 * toAddress
+	 *
+	 * @return  the toAddress
+	 * @since   CodingExample Ver 1.0
+	*/
+	
+	public String getToAddress() {
+		return toAddress;
+	}
+
+	/**
+	 * toAddress
+	 *
+	 * @param   toAddress    the toAddress to set
+	 * @since   CodingExample Ver 1.0
+	 */
+	
+	public void setToAddress(String toAddress) {
+		this.toAddress = toAddress;
+	}
+
+	/**
+	 * sendText
+	 *
+	 * @return  the sendText
+	 * @since   CodingExample Ver 1.0
+	*/
+	
+	public String getSendText() {
+		return sendText;
+	}
+
+	/**
+	 * sendText
+	 *
+	 * @param   sendText    the sendText to set
+	 * @since   CodingExample Ver 1.0
+	 */
+	
+	public void setSendText(String sendText) {
+		this.sendText = sendText;
+	}
+	
+	@Override
+	public String toString() {
+		
+		// TODO Auto-generated method stub
+		return JSON.toJSONString(this);
+		
+	}
+	
+	@Override
+	public int hashCode() {
+		
+		// TODO Auto-generated method stub
+		return formAddress.hashCode()+toAddress.hashCode();
+		
+	}
+}
---
+++ fnsco-core/src/main/java/net/fnsco/core/mail/MailSendService.java
@@ -0,0 +1,88 @@
+package net.fnsco.core.mail;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import com.google.common.base.Strings;
+
+import net.fnsco.core.base.BaseService;
+import net.fnsco.core.utils.TaskUtils;
+
+/**
+ * @desc 邮件发送service类
+ * @author   tangliang
+ * @version  0.0.1-SNAPSHOT
+ * @since    Ver 1.1
+ * @Date	 2018年1月23日 下午3:52:36
+ */
+@Service
+public class MailSendService extends BaseService{
+	
+	@Autowired
+	private JavaMailSender mailSender;
+	
+	/**
+	 * sendSystemMail:(发送系统邮件，发送方为公司)
+	 *
+	 * @param  @param params    设定文件
+	 * @return void    DOM对象
+	 * @author tangliang
+	 * @date   2018年1月23日 下午4:26:16
+	 */
+	public void sendSystemMail(MailSendParams params) {
+		params.setNick("杭州法奈昇科技有限公司");
+//		params.setFormAddress("");
+		sendMail(params);
+	}
+	
+	/**
+	 * sendMail:(发送邮件)
+	 *
+	 * @param  MailSendParams params    参数MailSendParams中的sendText属性 可以为文本，可以为html字符串 不能为文件类型
+	 * @return void    DOM对象
+	 * @author tangliang
+	 * @date   2018年1月23日 下午4:22:11
+	 */
+	public void sendMail(MailSendParams params) {
+		
+		if(Strings.isNullOrEmpty(params.getToTarget())||Strings.isNullOrEmpty(params.getToAddress())) {
+			logger.error("发送邮件参数错误!"+params);
+			return ;
+		}
+		/**
+		 * 异步发送邮件
+		 */
+		TaskUtils.execAsyn(new Runnable() {
+			@Override
+			public void run() {
+				MimeMessage message = null;
+				String nick = "";
+				if(!Strings.isNullOrEmpty(params.getNick())) {
+					try {
+						nick = javax.mail.internet.MimeUtility.encodeText(params.getNick());
+					} catch (Exception e) {
+						logger.error("发送短信nick编码错误!", e);
+					}
+				}
+				
+				try {
+					message = mailSender.createMimeMessage();
+					MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+					helper.setFrom(new InternetAddress(nick + " <" + params.getToTarget() + ">"));
+					helper.setTo(params.getToAddress());
+					helper.setSubject(params.getSendSubject());
+					helper.setText(params.getSendText(), true);
+				} catch (MessagingException e) {
+					logger.error("邮件发送失败");
+				}
+				mailSender.send(message);
+			}
+		});
+    }
+}
