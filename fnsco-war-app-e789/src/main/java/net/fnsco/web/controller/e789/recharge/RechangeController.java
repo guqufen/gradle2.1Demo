ce40219790fc48fe68dc9054f4bc62843648e1d8
liang.tang <liang.tang@zheft.cn>
2018-02-01 17:43:49 +0800

    提交代码

--- fnsco-war-app-e789/src/main/java/net/fnsco/web/controller/e789/recharge/RechangeController.java
+++ fnsco-war-app-e789/src/main/java/net/fnsco/web/controller/e789/recharge/RechangeController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSON;
@@ -21,8 +20,8 @@ import com.google.common.collect.Maps;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import net.fnsco.core.alipay.AlipayClientUtil;
 import net.fnsco.core.alipay.AlipayAppPayRequestParams;
+import net.fnsco.core.alipay.AlipayClientUtil;
 import net.fnsco.core.base.BaseController;
 import net.fnsco.core.base.ResultDTO;
 import net.fnsco.core.utils.DateUtils;
