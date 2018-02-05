e65d47255278eacda79c2ec225b8e6d6a3ccf515
hjt <jinting.he@zheft.cn>
2018-02-05 09:47:01 +0800

    更新

--- fnsco-war-app-e789/src/main/java/net/fnsco/web/controller/e789/open/pay/AlipayNotifyController.java
+++ fnsco-war-app-e789/src/main/java/net/fnsco/web/controller/e789/open/pay/AlipayNotifyController.java
@@ -1,5 +1,6 @@
 package net.fnsco.web.controller.e789.open.pay;
 
+import java.math.BigDecimal;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alipay.api.domain.AlipayTradeRefundModel;
 import com.alipay.api.response.AlipayTradeRefundResponse;
 
 import io.swagger.annotations.Api;
@@ -85,6 +87,7 @@ public class AlipayNotifyController extends BaseController{
 		//交易超时未付款或关闭
 		if("TRADE_CLOSED".equals(tradeStatus)) {
 			tradeWithdrawService.doAlipayRechangeNotify(params, false, tradeWithdraw);
+			return "success";
 		}
 		
 		boolean tradeStatusVali = AlipayClientUtil.checkTradeStatue(params);
@@ -102,6 +105,7 @@ public class AlipayNotifyController extends BaseController{
      */
     @RequestMapping(value = "/ticketPayNotify")
 	@ApiOperation(value = "支付宝APP火车票支付异步通知接口，仅支付宝方调用")
+    @ResponseBody
     public String ticketPayNotify() {
     	Map<String,Object> rsaMap = AlipayClientUtil.rsaCheckV1(request);
 		boolean flag = (boolean) rsaMap.get("signature");
@@ -112,30 +116,58 @@ public class AlipayNotifyController extends BaseController{
 			logger.error("本次调用非正常调用!");
 			return "fail";
 		}
+		
 		/**
 		 * 在认证是支付宝发来的数据后，接下来处理业务
 		 */
 		Map<String,String> params = (Map<String, String>) rsaMap.get("params");
-		boolean tradeStatus = AlipayClientUtil.checkTradeStatue(params);
+		String orderNo = params.get("out_trade_no");
+		TradeWithdrawDO tradeWithdraw = tradeWithdrawService.getByOrderNo(orderNo);
+		String tradeStatus = params.get("trade_status");
 		
-		if(!tradeStatus) {
+		
+		if(null == tradeWithdraw) {
+			logger.error("该订单已经不存在，不处理!orderNo="+orderNo);
+			return "success";
+		}
+		
+		/**
+		 * 处理完成的订单，不处理
+		 */
+		if(tradeWithdraw.getStatus() == WithdrawStateEnum.SUCCESS.getCode()) {
+			logger.error("该订单已经处理过，不处理!orderNo="+orderNo);
+			return "success";
+		}
+		//交易超时未付款或关闭
+		if("TRADE_CLOSED".equals(tradeStatus)) {
+			tradeWithdrawService.doAlipayRechangeNotify(params, false, tradeWithdraw);
+			return "success";
+		}
+		
+		boolean tradeStatusVali = AlipayClientUtil.checkTradeStatue(params);
+		if(!tradeStatusVali) {
 			logger.error("该订单非完成支付状态，不处理!orderNo="+params.get("out_trade_no"));
 			return "fail";
 		}
+		//成功处理
 		
-		String orderNo = params.get("out_trade_no");
 		ResultDTO result = ticketOrderService.payByZFBNotify(orderNo);
     	if(result.isSuccess()) {
     		return "success";
     	}else {
     	AlipayRefundRequestParams requestParams = new AlipayRefundRequestParams();
-    	requestParams.setRefundReason("");
-    	requestParams.setRefundAmount("");
-    	requestParams.setOutTradeNo(orderNo);
-    	/*requestParams.setNotifyUrl("");
-    	AlipayTradeRefundResponse body = AlipayClientUtil.createTradeReturnOrderParams(requestParams);
-    	return body;*/
-    	return null;
+    	
+		requestParams.setRefundAmount(String.format("%.2f", tradeWithdraw.getOrderAmount()));
+		requestParams.setRefundReason("火车票购买失败退款");
+		requestParams.setOutTradeNo(orderNo);
+		AlipayTradeRefundResponse response = AlipayClientUtil.createTradeReturnOrderParams(requestParams);
+		if(response.isSuccess()) {
+			logger.error("该订单退款成功!orderNo="+requestParams.getOutTradeNo()+",退款金额为:"+requestParams.getRefundAmount());
+		}
     	}
+		
+		tradeWithdrawService.doAlipayRechangeNotify(params, true, tradeWithdraw);
+		return "success";
+		
     }
 }
