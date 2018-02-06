c9a7976d17d2462ae7c1467dd998673a99d4c086
yx <yx>
2018-02-06 11:37:35 +0800

    手机充值 优化支付宝回调

--- fnsco-service-trading/src/main/java/net/fnsco/trading/service/third/reCharge/PrepaidRefillService.java
+++ fnsco-service-trading/src/main/java/net/fnsco/trading/service/third/reCharge/PrepaidRefillService.java
@@ -13,17 +13,21 @@ import org.springframework.stereotype.Service;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.alipay.api.response.AlipayTradeRefundResponse;
 
 import net.fnsco.bigdata.comm.ServiceConstant.TradeStateEnum;
 import net.fnsco.core.alipay.AlipayAppPayRequestParams;
 import net.fnsco.core.alipay.AlipayClientUtil;
+import net.fnsco.core.alipay.AlipayRefundRequestParams;
 import net.fnsco.core.base.BaseService;
 import net.fnsco.core.base.ResultDTO;
 import net.fnsco.core.utils.CodeUtil;
+import net.fnsco.core.utils.DateUtils;
 import net.fnsco.core.utils.HttpUtils;
 import net.fnsco.freamwork.comm.Md5Util;
 import net.fnsco.trading.comm.TradeConstants;
 import net.fnsco.trading.comm.TradeConstants.WithdrawStateEnum;
+import net.fnsco.trading.comm.TradeConstants.thrRechargeStateEnum;
 import net.fnsco.trading.service.account.AppAccountBalanceService;
 import net.fnsco.trading.service.third.reCharge.dto.ChargeDTO;
 import net.fnsco.trading.service.third.reCharge.dto.ChargeResultDTO;
@@ -955,23 +959,6 @@ public class PrepaidRefillService extends BaseService {
 			// 话费充值
 			if (0 == chargeDTO.getType()) {
 
-				// // 先通过pid查询实际售价
-				// String amt = queryCharge(chargeDTO.getPhone(),
-				// Integer.parseInt(chargeDTO.getPid()));
-				// if (amt == null) {
-				// logger.error("无此套餐");
-				// juhe1.setError_code(null);
-				// juhe1.setReason("无此套餐");
-				// return juhe1;
-				// }
-				//
-				// if (!amt.equals(chargeDTO.getInprice())) {
-				// logger.error("套餐售价金额与app端传入参不一致，交易失败");
-				// juhe1.setError_code(null);
-				// juhe1.setReason("套餐售价金额与app端传入参不一致，交易失败");
-				// return juhe1;
-				// }
-
 				// md5,校验值，md5(OpenID+key+phone+pid+orderid)，结果转为小写
 				String sign = Md5Util.MD5(env.getProperty("jh.phone.OpenId") + env.getProperty("jh.phone.feekey")
 						+ chargeDTO.getPhone() + chargeDTO.getPid() + orderId).toLowerCase();
@@ -983,24 +970,6 @@ public class PrepaidRefillService extends BaseService {
 
 				// 流量充值
 			} else {
-
-				// 先通过pid查询实际售价
-				// String amt = queryFlowExpen(chargeDTO.getPhone(),
-				// chargeDTO.getPid());
-				//
-				// if (amt == null) {
-				// logger.error("无此套餐");
-				// juhe1.setError_code(null);
-				// juhe1.setReason("无此套餐");
-				// return juhe1;
-				// }
-				//
-				// if (!amt.equals(chargeDTO.getInprice())) {
-				// logger.error("套餐售价金额与app端传入参不一致，交易失败");
-				// juhe1.setError_code(null);
-				// juhe1.setReason("套餐售价金额与app端传入参不一致，交易失败");
-				// return juhe1;
-				// }
 
 				// md5,校验值，md5(OpenID+key+phone+pid+orderid)，结果转为小写
 				String sign = Md5Util.MD5(env.getProperty("jh.phone.OpenId") + env.getProperty("jh.phone.flowkey")
@@ -1137,5 +1106,179 @@ public class PrepaidRefillService extends BaseService {
 		chargeResultDTO.setOrderNo(orderNo);
 		logger.info("alipay->body:" + body);
 		return ResultDTO.success(chargeResultDTO);
+	}
+
+	/**
+	 * 支付宝回调之后，继续处理充值
+	 * 
+	 * @param params
+	 * @param isSuccess
+	 * @param reChargeOrderDO
+	 */
+	public void doAlipayThrChangeNotify(Map<String, String> params, Boolean isSuccess,
+			RechargeOrderDO reChargeOrderDO) {
+
+		ChargeDTO chargeDTO = new ChargeDTO();
+		String payName = null;
+		String orderNo = params.get("out_trade_no");
+//		String status = params.get("trade_status");// 交易状态
+
+		// 订单表查找订单号交易
+		TradeWithdrawDO tradeWithdrawDO = tradeWithdrawService.getByOrderNo(orderNo);// 通过订单号查找原交易
+		// 订单号流水表为空,则需要将recharge表数据复制给withdraw，然后插表
+		if (null == tradeWithdrawDO) {
+
+			logger.error("支付宝回调通知函数，订单号在表中找不到原交易，orderNo=[" + orderNo + "]");
+			tradeWithdrawDO = new TradeWithdrawDO();
+			tradeWithdrawDO.setOrderNo(orderNo);// 设置订单号
+			tradeWithdrawDO.setAmount(new BigDecimal(reChargeOrderDO.getAmt()));// 设置交易金额，优惠金额
+			tradeWithdrawDO.setAppUserId(chargeDTO.getUserId());// 设置帐号ID
+			tradeWithdrawDO.setTradeType(2);// 交易类型:2-消费
+			if ("0" == reChargeOrderDO.getType()) {
+				tradeWithdrawDO.setTradeSubType(TradeConstants.TxnSubTypeEnum.BUY_HF.getCode());// 交易子类型:22话费充值
+				payName = "话费充值";
+			} else {
+				tradeWithdrawDO.setTradeSubType(TradeConstants.TxnSubTypeEnum.BUY_LT.getCode());// 交易子类型:23流量充值
+				payName = "流量充值";
+			}
+			tradeWithdrawDO.setStatus(WithdrawStateEnum.PROCESSING.getCode());// 设置交易状态，1-执行中
+			tradeWithdrawService.doAdd(tradeWithdrawDO);// 更新数据库表
+		}
+
+		if (!isSuccess) {
+
+			logger.error(payName + "-支付宝扣款交易失败");
+
+			RechargeOrderDO phoneChargeOrderDO1 = new RechargeOrderDO();
+			phoneChargeOrderDO1.setId(reChargeOrderDO.getId());// 设置ID
+			phoneChargeOrderDO1.setRespCode(TradeConstants.RespCodeEnum.FAIL.getCode());// 设置应答码
+			phoneChargeOrderDO1.setRespMsg(payName + "-支付宝扣款交易失败");// 设置应答信息
+			phoneChargeOrderDO1.setStatus(WithdrawStateEnum.FAIL.getCode());// 设置状态码
+			rechargeOrderService.doUpdate(phoneChargeOrderDO1);
+
+			TradeWithdrawDO tradeWithdraw1 = new TradeWithdrawDO();
+			tradeWithdraw1.setId(tradeWithdrawDO.getId());
+			tradeWithdraw1.setRespCode(TradeConstants.RespCodeEnum.FAIL.getCode());// 设置应答码
+			tradeWithdraw1.setRespMsg(payName + "-支付宝扣款交易失败");// 设置应答信息
+			tradeWithdraw1.setStatus(WithdrawStateEnum.FAIL.getCode());// 设置状态码
+			tradeWithdrawService.doUpdate(tradeWithdraw1);
+
+		} else {
+
+			Date gmtPayment = DateUtils.toParseYmdhms(params.get("gmt_payment"));
+			Date notifyTime = DateUtils.toParseYmdhms(params.get("notify_time"));
+
+			// 回调时间大于付款时间15分钟(time想减是毫秒数，除以1000变为秒，再除以60变为分)
+			if (15 < ((notifyTime.getTime() - gmtPayment.getTime()) / (1000 * 60))) {
+
+				logger.error(payName + "-支付宝扣款回调超时," + "支付时间=[" + params.get("gmt_payment") + "],回调时间=["
+						+ params.get("notify_time") + "],准备扣款");
+
+				// 回调超时,退款
+				AlipayRefundRequestParams requestParams = new AlipayRefundRequestParams();
+				requestParams.setTradeNo(orderNo);// 设置订单号
+				AlipayTradeRefundResponse alipayTradeRefundResponse = AlipayClientUtil
+						.createTradeReturnOrderParams(requestParams);
+
+				logger.info("支付宝退款返回：" + JSON.toJSONString(alipayTradeRefundResponse));
+
+				// 调用成功
+				if ("Y".equals(alipayTradeRefundResponse.getFundChange()) && alipayTradeRefundResponse.isSuccess()) {
+
+					logger.error(payName + "退款成功,orderno=[" + orderNo + "]");
+
+					// 调用失败
+				} else {
+
+					logger.error(payName + "退款失败,orderno=[" + orderNo + "]");
+				}
+
+				TradeWithdrawDO tradeWithdraw1 = new TradeWithdrawDO();
+				tradeWithdraw1.setId(tradeWithdrawDO.getId());
+				tradeWithdraw1.setRespCode(TradeConstants.RespCodeEnum.FAIL.getCode());// 设置应答码
+				tradeWithdraw1.setRespMsg(payName + "-支付宝扣款回调超时");// 设置应答信息
+				tradeWithdraw1.setStatus(WithdrawStateEnum.FAIL.getCode());// 设置状态码
+				tradeWithdrawService.doUpdate(tradeWithdraw1);
+
+				RechargeOrderDO phoneChargeOrderDO1 = new RechargeOrderDO();
+				phoneChargeOrderDO1.setId(reChargeOrderDO.getId());// 设置ID
+				phoneChargeOrderDO1.setRespCode(TradeConstants.RespCodeEnum.FAIL.getCode());// 设置应答码
+				phoneChargeOrderDO1.setRespMsg(payName + "-支付宝扣款回调超时");// 设置应答信息
+				phoneChargeOrderDO1.setStatus(WithdrawStateEnum.FAIL.getCode());// 设置状态码
+				rechargeOrderService.doUpdate(phoneChargeOrderDO1);
+				return;
+			}
+
+			logger.info("手机充值-支付宝回调，支付成功，继续进行" + payName);
+
+			// 手机充值
+			chargeDTO.setType(Integer.parseInt(reChargeOrderDO.getType()));// 设置类型
+			chargeDTO.setPid(tradeWithdrawDO.getOriginalOrderNo());// 设置套餐ID
+			chargeDTO.setPhone(reChargeOrderDO.getMobile());// 充值手机号
+
+			RechargeOrderDO phoneChargeOrderDO1 = new RechargeOrderDO();
+			TradeWithdrawDO tradeWithdrawDO1 = new TradeWithdrawDO();
+			phoneChargeOrderDO1.setId(reChargeOrderDO.getId());
+			tradeWithdrawDO1.setId(tradeWithdrawDO.getId());
+
+			// 充值交易调用
+			JuheDTO juhe = recharge(chargeDTO, orderNo);
+			if (juhe.getError_code() == 0) {
+
+				Map<String, Object> map = JSONObject.parseObject(juhe.getResult().toString(), Map.class);
+
+				phoneChargeOrderDO1.setPayOrderNo(map.get("sporder_id").toString());// 设置聚合订单号
+				phoneChargeOrderDO1.setRespCode(TradeConstants.RespCodeEnum.HANDLING.getCode());// 订单提交成功，等待充值
+				phoneChargeOrderDO1.setRespMsg(juhe.getReason());// 设置响应
+				tradeWithdrawDO1.setOriginalOrderNo(map.get("sporder_id").toString());// 设置渠道订单号
+				tradeWithdrawDO1.setRespCode(TradeConstants.RespCodeEnum.HANDLING.getCode());// 订单提交成功，等待充值需要再次调用订单查询接口进行查询
+				tradeWithdrawDO1.setUpdateTime(new Date());// 设置交易完成时间
+				tradeWithdrawDO1.setRespMsg(juhe.getReason());// 设置响应
+
+				// 系统内部异常(调用充值类业务时，请务必联系客服或通过订单查询接口检测订单，避免造成损失)
+			} else if (juhe.getError_code() == 10014) {
+
+				phoneChargeOrderDO1.setRespCode(TradeConstants.RespCodeEnum.HANDLING.getCode());// 交易进行中
+				phoneChargeOrderDO1.setRespMsg(juhe.getReason());// 设置响应
+				phoneChargeOrderDO1.setRespCode(TradeConstants.RespCodeEnum.HANDLING.getCode());// 订单提交成功，等待充值
+
+				tradeWithdrawDO1.setRespCode(TradeConstants.RespCodeEnum.HANDLING.getCode());// 交易进行中，需要再次调用订单查询接口进行查询
+				tradeWithdrawDO1.setRespMsg(juhe.getReason());// 设置响应
+				tradeWithdrawDO1.setUpdateTime(new Date());// 设置最后更新时间
+
+				// 充值失败
+			} else {
+
+				phoneChargeOrderDO1.setRespCode(TradeConstants.RespCodeEnum.FAIL.getCode());// 交易失败
+				phoneChargeOrderDO1.setRespMsg(juhe.getReason());// 设置响应
+				phoneChargeOrderDO1.setStatus(WithdrawStateEnum.FAIL.getCode());// 状态为2-失败
+
+				tradeWithdrawDO1.setRespCode(TradeConstants.RespCodeEnum.FAIL.getCode());// 交易进行中，需要再次调用订单查询接口进行查询
+				tradeWithdrawDO1.setStatus(WithdrawStateEnum.FAIL.getCode());// 状态为2-失败
+				tradeWithdrawDO1.setRespMsg(juhe.getReason());// 设置响应
+				tradeWithdrawDO1.setUpdateTime(new Date());// 设置最后更新时间
+
+				// 充值失败,退款
+				AlipayRefundRequestParams requestParams = new AlipayRefundRequestParams();
+				requestParams.setTradeNo(orderNo);// 设置订单号
+				AlipayTradeRefundResponse alipayTradeRefundResponse = AlipayClientUtil
+						.createTradeReturnOrderParams(requestParams);
+
+				// 退款成功且金额发生变动
+				if ("Y".equals(alipayTradeRefundResponse.getFundChange()) && alipayTradeRefundResponse.isSuccess()) {
+
+					logger.error(payName + "退款成功,orderno=[" + orderNo + "]");
+
+					// 调用失败
+				} else {
+
+					logger.error(payName + "退款失败,orderno=[" + orderNo + "]");
+				}
+			}
+
+			// 更新数据
+			tradeWithdrawService.doUpdate(tradeWithdrawDO1);
+			rechargeOrderService.doUpdate(phoneChargeOrderDO1);
+		}
 	}
 }
