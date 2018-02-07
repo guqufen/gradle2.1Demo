8168f3dce2b6e9b9c0c8043ae289f0ff9990f980
sxf <xianfei.song@zheft.cn>
2018-02-07 11:23:28 +0800

    session共享删除

--- fnsco-framework/src/main/java/net/fnsco/freamwork/session/SessionInitializer.java
+++
@@ -1,9 +0,0 @@
-package net.fnsco.freamwork.session;
-
-import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
-
-public class SessionInitializer extends AbstractHttpSessionApplicationInitializer {
-    public SessionInitializer() {
-        super(RedisSessionConfig.class);
-    }
-}
