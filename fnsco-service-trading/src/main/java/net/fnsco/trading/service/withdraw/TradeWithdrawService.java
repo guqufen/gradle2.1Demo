6a6746b5b6abc7716ad9a27e5349004ed02b1b7a
sxf <xianfei.song@zheft.cn>
2018-02-09 15:20:22 +0800

    火车票退票

--- fnsco-service-trading/src/main/java/net/fnsco/trading/service/withdraw/TradeWithdrawService.java
+++ fnsco-service-trading/src/main/java/net/fnsco/trading/service/withdraw/TradeWithdrawService.java
@@ -234,7 +234,7 @@ public class TradeWithdrawService extends BaseService {
         TradeWithdrawDO tradeWithdraw = new TradeWithdrawDO();
         tradeWithdraw.setAmount(order.getOrderAmount());
         tradeWithdraw.setAppUserId(order.getAppUserId());
-        tradeWithdraw.setOrderNo(order.getOrderNo());
+        tradeWithdraw.setOriginalOrderNo(order.getOrderNo());
         // tradeWithdraw.setChannelMerId(channelMerId);
         // tradeWithdraw.setFee(fee);
         tradeWithdraw.setFund(appAccountBalance.getFund());
