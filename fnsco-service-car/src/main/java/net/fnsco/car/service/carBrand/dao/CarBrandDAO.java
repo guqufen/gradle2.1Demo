dbd8a8a9a49d81ba1533e88e1c82e7c16913c9bd
yx <yx>
2018-02-24 17:01:06 +0800

    优化，中融汽车异步数据加载

--- fnsco-service-car/src/main/java/net/fnsco/car/service/carBrand/dao/CarBrandDAO.java
+++ fnsco-service-car/src/main/java/net/fnsco/car/service/carBrand/dao/CarBrandDAO.java
@@ -30,20 +30,20 @@ public interface CarBrandDAO {
 	@Insert("INSERT INTO car_dic_type(id, name, supper_id, level, icon_img_path, model,is_hot) values(#{id},#{name},#{supperId},#{level},#{iconImgPath},#{model},#{isHot})")
 	@Options(useGeneratedKeys = true, keyProperty = "id")
 	public void insert(CarBrandDO carBrandDO);
-	
+
 	@SelectProvider(type = CarBrandProvider.class, method = "update")
 	public void update(@Param("carBrandDO") CarBrandDO carBrandDO);
-	
+
 	@Delete("delete from car_dic_type where id = #{id}")
 	public void delete(CarBrandDO carBrandDO);
 
 	@Results({ @Result(column = "id", property = "id"), @Result(column = "name", property = "name"),
 			@Result(column = "supper_id", property = "supperId"), @Result(column = "level", property = "level"),
 			@Result(column = "icon_img_path", property = "iconImgPath"), @Result(column = "model", property = "model"),
-			@Result(column = "is_hot", property = "isHot") })
-	@Select("select * from car_dic_type where level='1' order by CONVERT(name USING gbk) asc")
+			@Result(column = "childCount", property = "childCount"), @Result(column = "is_hot", property = "isHot") })
+	@Select("select r.*, (select count(*) from car_dic_type t where t.supper_id = r.id) as childCount from car_dic_type r where level='1' order by CONVERT(name USING gbk) asc")
 	public List<CarBrandDO> selectAllFirstLevel();
-	
+
 	@Results({ @Result(column = "id", property = "id"), @Result(column = "name", property = "name"),
 			@Result(column = "supper_id", property = "supperId"), @Result(column = "level", property = "level"),
 			@Result(column = "icon_img_path", property = "iconImgPath"), @Result(column = "model", property = "model"),
@@ -54,12 +54,14 @@ public interface CarBrandDAO {
 	@Results({ @Result(column = "id", property = "id"), @Result(column = "name", property = "name"),
 			@Result(column = "supper_id", property = "supperId"), @Result(column = "level", property = "level"),
 			@Result(column = "icon_img_path", property = "iconImgPath"), @Result(column = "model", property = "model"),
-			@Result(column = "is_hot", property = "isHot"), @Result(column = "supperName", property = "supperName")})
+			@Result(column = "is_hot", property = "isHot"), @Result(column = "supperName", property = "supperName") })
 	@SelectProvider(type = CarBrandProvider.class, method = "selectByCondition")
-	public List<CarBrandDO> selectByCondition(@Param("carBrandDO") CarBrandDO carBrandDO, @Param("limit") Integer limit);
+	public List<CarBrandDO> selectByCondition(@Param("carBrandDO") CarBrandDO carBrandDO,
+			@Param("limit") Integer limit);
 
 	/**
 	 * 多级查找父id关联
+	 * 
 	 * @param id
 	 * @return
 	 */
@@ -69,36 +71,39 @@ public interface CarBrandDAO {
 			@Result(column = "is_hot", property = "isHot") })
 	@Select("select * from car_dic_type where supper_id = #{id} or supper_id in (select distinct id from car_dic_type where supper_id =#{id}) or supper_id in (select distinct id from car_dic_type where supper_id in (select distinct id from car_dic_type where supper_id =#{id})) or id =#{id} order by id")
 	public List<CarBrandDO> selectChild(Integer id);
-	
+
 	/**
 	 * 单级查找父id关联
+	 * 
 	 * @param id
 	 * @return
 	 */
 	@Results({ @Result(column = "id", property = "id"), @Result(column = "name", property = "name"),
 			@Result(column = "supper_id", property = "supperId"), @Result(column = "level", property = "level"),
 			@Result(column = "icon_img_path", property = "iconImgPath"), @Result(column = "model", property = "model"),
-			@Result(column = "is_hot", property = "isHot") })
-	@Select("select * from car_dic_type where supper_id = #{id} order by id")
+			@Result(column = "childCount", property = "childCount"), @Result(column = "is_hot", property = "isHot") })
+	@Select("select r.*, (select count(*) from car_dic_type t where t.supper_id = r.id) as childCount from car_dic_type r where supper_id = #{id} order by id")
 	public List<CarBrandDO> selectChildById(Integer id);
-	
+
 	/**
 	 * getById:(根据ID查找)
 	 *
-	 * @param  @param id
-	 * @param  @return    设定文件
-	 * @return CarBrandDO    DOM对象
+	 * @param @param
+	 *            id
+	 * @param @return
+	 *            设定文件
+	 * @return CarBrandDO DOM对象
 	 * @author tangliang
-	 * @date   2017年12月11日 下午3:56:16
+	 * @date 2017年12月11日 下午3:56:16
 	 */
 	@Results({ @Result(column = "id", property = "id"), @Result(column = "name", property = "name"),
-		@Result(column = "supper_id", property = "supperId"), @Result(column = "level", property = "level"),
-		@Result(column = "icon_img_path", property = "iconImgPath"), @Result(column = "model", property = "model"),
-		@Result(column = "is_hot", property = "isHot") })
+			@Result(column = "supper_id", property = "supperId"), @Result(column = "level", property = "level"),
+			@Result(column = "icon_img_path", property = "iconImgPath"), @Result(column = "model", property = "model"),
+			@Result(column = "is_hot", property = "isHot") })
 	@Select("SELECT * FROM car_dic_type WHERE id = #{id}")
 	public CarBrandDO getById(@Param("id") int id);
 
-	@Results({ @Result(column = "id", property = "id"), @Result(column = "name", property = "name"),})
+	@Results({ @Result(column = "id", property = "id"), @Result(column = "name", property = "name"), })
 	@Select("SELECT id,name FROM car_dic_type WHERE supper_id = '0'")
 	public List<CarBrandDO> getFirstLevel();
 }
