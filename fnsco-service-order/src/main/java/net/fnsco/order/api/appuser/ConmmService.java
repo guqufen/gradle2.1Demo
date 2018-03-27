5b35c998ec987d7502bdb6b5d271fd25f825656e
912664220@qq.com <wyyhjt1314>
2018-03-27 11:11:05 +0800

    更新

--- fnsco-service-order/src/main/java/net/fnsco/order/api/appuser/ConmmService.java
+++ fnsco-service-order/src/main/java/net/fnsco/order/api/appuser/ConmmService.java
@@ -3,6 +3,7 @@ package net.fnsco.order.api.appuser;
 import java.util.Map;
 
 import net.fnsco.core.base.ResultDTO;
+import net.fnsco.core.utils.FileUtils;
 import net.fnsco.order.api.dto.ProtocolDTO;
 import net.fnsco.order.api.dto.SuggestDTO;
 import net.fnsco.order.api.dto.VersionDTO;
@@ -13,7 +14,7 @@ public interface ConmmService {
     ResultDTO<VersionResultDTO> queryLastVersionInfo(VersionDTO sysVersionDTO);
     //获取用户协议
     ResultDTO<Map<String, String>> getProtocol(ProtocolDTO protocolDTO);
-    //获取数钱吧用户协议
+    //获取易账房用户协议
     ResultDTO<Map<String, String>> getBigDataProtocol(ProtocolDTO protocolDTO);
     //获取拉卡拉用户协议
     ResultDTO<Map<String, String>> getLklProtocol(ProtocolDTO protocolDTO);
